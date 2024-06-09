f5ae8bf2a1c90043d21d8d54dc5af31139cce85f
Cows Carrying Cakes and Candles <6robot@getdown.town>
2024-06-09 15:24:36 -0500

    add app config files

---
+++ vite.config.ts
@@ -0,0 +1,70 @@
+
+// yarn add --dev @esbuild-plugins/node-globals-polyfill
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+// yarn add --dev @esbuild-plugins/node-modules-polyfill
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+// You don't need to add this to deps, it's included by @esbuild-plugins/node-modules-polyfill
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
+
+import react from "@vitejs/plugin-react"
+
+export default {
+    resolve: {
+        alias: {
+            // This Rollup aliases are extracted from @esbuild-plugins/node-modules-polyfill,
+            // see https://github.com/remorses/esbuild-plugins/blob/master/node-modules-polyfill/src/polyfills.ts
+            // process and buffer are excluded because already managed
+            // by node-globals-polyfill
+            stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+            path: 'rollup-plugin-node-polyfills/polyfills/path',
+            crypto: 'rollup-plugin-node-polyfills/polyfills/crypto'
+        }
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            // Node.js global to browser globalThis
+            define: {
+                global: 'globalThis'
+            },
+            // Enable esbuild polyfill plugins
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    process: true,
+                    buffer: true
+                }),
+                NodeModulesPolyfillPlugin()
+            ]
+        }
+    },
+    build: {
+        rollupOptions: {
+            plugins: [
+                // Enable rollup polyfills plugin
+                // used during production bundling
+                rollupNodePolyFill()
+            ]
+        },
+        outDir: "build",
+        sourcemap: true
+    },
+    plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      strictPort: true,
+      host: true,
+      port: 3005,
+    },
+    preview: {
+      open: true,
+      host: true,
+      port: 3005,
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "src/setupTests",
+      mockReset: true,
+    },
+}
