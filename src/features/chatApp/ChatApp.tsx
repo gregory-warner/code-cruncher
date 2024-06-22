61f6dd68b1106a2f9be71cf56b51fcf12b15f33d
Cats Carrying Canes <6robot@getdown.town>
2024-06-21 22:50:14 -0500

    remove default chat load

--- src/features/chatApp/ChatApp.tsx
+++ src/features/chatApp/ChatApp.tsx
@@ -1,33 +1,11 @@
-import { useEffect } from 'react';
 import Conversation from '../conversation/Conversation';
 import { Box, Grid } from '@mui/material';
 import UserInput from '../user/UserInput';
 import ActorDrawer from '../actorDrawer/ActorDrawer';
-import { useAppDispatch } from '../../store/hooks';
-import {updateDialog} from '../conversation/store/conversationSlice';
-import { defaultActor} from '../actor/actorSlice';
 import ChatAppHeader from './components/ChatAppHeader';
-import {defaultUser} from '../user/userSlice';
 import ActorConfigDrawer from '../actorConfigDrawer/ActorConfigDrawer';
-import {useGetAssistantQuery, useGetUserQuery} from "../../services/server/serverApi";
 
 const ChatApp = () => {
-    const dispatch = useAppDispatch();
-
-    const { data: userData } = useGetUserQuery(defaultUser);
-    const { data: actorData } = useGetAssistantQuery(defaultActor);
-
-    useEffect(() => {
-        if (!userData || !actorData) {
-            return;
-        }
-
-        dispatch(updateDialog({
-            user: userData,
-            actor: actorData,
-        }));
-
-    }, [userData, actorData]);
 
     return (
         <Grid container direction="column" sx={{ height: "100vh", overflowY: "auto"}}>
