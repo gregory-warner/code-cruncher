242f736ecdc1c0da5d51ad08681ff649516c6f1b
Greg Warner <[insert email here]>
2024-07-30 23:32:51 -0500

    set offline models first in list

--- src/features/actorCreationDrawer/hooks/useModels.tsx
+++ src/features/actorCreationDrawer/hooks/useModels.tsx
@@ -2,15 +2,14 @@ import {useGetModelsQuery} from "../../../services/openai/openaiApi";
 import {useModelsQuery} from "../../../services/ollama/ollamaApi";
 
 const useModels = () => {
-    debugger;
-    const { data: models } = useGetModelsQuery();
     const { data: ollama } = useModelsQuery();
+    const { data: models } = useGetModelsQuery();
 
     if (!models || !ollama) return [];
 
     return [
+        ...ollama.models,
         ...models.data,
-        ...ollama.models.map((model) => ({ name: `${model.name} (offline)` })),
     ];
 };
 
