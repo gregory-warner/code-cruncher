b43deb31a00c54ef484d51362679a35f7afa8a32
Greg Warner <[insert email here]>
2024-07-30 22:35:16 -0500

    add ollama api

---
+++ src/services/ollama/ollamaApi.ts
@@ -0,0 +1,75 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {ollamaUrl} from "../../../config";
+import {ModelResponse, Model, ChatRequest, ChatResponse} from "./types";
+
+export const ollamaApi  = createApi({
+    reducerPath: 'ollamaApi',
+    baseQuery: fetchBaseQuery({ baseUrl: ollamaUrl,}),
+    tagTypes: ['Model'],
+    endpoints: (build) => ({
+        unloadModel: build.query<ModelResponse, string>({
+            query: (model: string) =>  ({
+                url:  `api/generate`,
+                method: 'POST',
+                body: {
+                    model,
+                    keep_alive: 0
+                },
+            }),
+        }),
+        persistModel: build.query<ModelResponse, string>({
+            query: (model: string) =>  ({
+                url:  `api/generate`,
+                method: 'POST',
+                body: {
+                    model,
+                    keep_alive: -1
+                },
+            }),
+        }),
+        show: build.query<ModelResponse, string>({
+            query: (model: string) =>  ({
+                url:  `api/show`,
+                method: 'POST',
+                body: {
+                    model,
+                },
+            }),
+        }),
+        heartbeat: build.query<string, string>({
+            query: () => ({
+                url: '/',
+                method: 'GET',
+            }),
+        }),
+        runningModels: build.query<Model[], null>({
+            query: () => ({
+                url: 'api/ps',
+                method: 'GET',
+            }),
+        }),
+        models: build.query<Model[], string>({
+            query: () => ({
+                url: 'api/tags',
+                method: 'GET',
+            }),
+        }),
+        chat: build.query<ChatResponse, ChatRequest>({
+            query: (request: ChatRequest) =>  ({
+                url:  `api/chat`,
+                method: 'POST',
+                body: request,
+            }),
+        }),
+    }),
+});
+
+export const {
+    useLazyUnloadModelQuery,
+    useLazyPersistModelQuery,
+    useLazyShowQuery,
+    useHeartbeatQuery,
+    useLazyRunningModelsQuery,
+    useLazyModelsQuery,
+    useChatQuery,
+} = ollamaApi;
---
+++ src/services/ollama/types.ts
@@ -0,0 +1,136 @@
+export interface LoadModel {
+    model: string;
+    keep_alive: 0|1|-1;
+}
+
+export interface ModelResponse {
+    model: string;
+    created_at: string;
+    response: string;
+    done: boolean;
+    done_reason: string;
+}
+
+interface ModelDetails {
+    parent_model: string;
+    format: string;
+    family: string;
+    families: string[];
+    parameter_size: string;
+    quantization_level: string;
+}
+
+export interface Model {
+    name: string;
+    model: string;
+    modified_at: string;
+    size: number;
+    digest: string;
+    details: ModelDetails;
+}
+
+type ImageData = ArrayBuffer | Uint8Array;
+
+interface ToolCallFunction {
+    name: string;
+    arguments: ToolCallFunctionArguments;
+}
+
+// ToolCallFunctionArguments is a map of strings to any type.
+interface ToolCallFunctionArguments {
+    [key: string]: any;
+}
+
+interface ToolCall {
+    function: ToolCallFunction
+}
+
+interface Tool {
+    type: string;
+    function: ToolFunction;
+}
+
+interface ToolFunction {
+    name: string;
+    description: string;
+    parameters: {
+        type: string;
+        required: string[];
+        properties: {
+            [key: string]: {
+                type: string;
+                description: string;
+                enum?: string[];
+            }
+        };
+    };
+}
+
+export interface Message {
+    role: string;
+    content: string;
+    images: ImageData[];
+    tool_calls: ToolCall[];
+}
+
+export interface ChatRequest {
+    model: string;
+    messages: Message[];
+    stream: boolean;
+    format: string;
+    keep_alive: 0|1|-1;
+    tools: Tool[];
+    options: Options;
+}
+
+interface Options {
+    runner?: Runner;
+
+    num_keep?: number;
+    seed?: number;
+    num_predict?: number;
+    top_k?: number;
+    top_p?: number;
+    min_p?: number;
+    tfs_z?: number;
+    typical_p?: number;
+    repeat_last_n?: number;
+    temperature?: number;
+    repeat_penalty?: number;
+    presence_penalty?: number;
+    frequency_penalty?: number;
+    mirostat?: number;
+    mirostat_tau?: number;
+    mirostat_eta?: number;
+    penalize_newline?: boolean;
+    stop?: string[];
+}
+
+interface Runner {
+    numa?: boolean;
+    num_ctx?: number;
+    num_batch?: number;
+    num_gpu?: number;
+    main_gpu?: number;
+    low_vram?: boolean;
+    f16_kv?: boolean;
+    logits_all?: boolean;
+    vocab_only?: boolean;
+    use_mmap?: boolean | null;
+    use_mlock?: boolean;
+    num_thread?: number;
+}
+
+export interface ChatResponse {
+    model: string;
+    created_at: Date;
+    message: Message;
+    done_reason?: string;
+    done: boolean;
+    total_duration?: number;
+    load_duration?: number;
+    prompt_eval_count?: number;
+    prompt_eval_duration?: number;
+    eval_count?: number;
+    eval_duration?: number;
+}
