309552d9c0b048c7aa30699be6c3862e79d01bc1
Greg Warner <[insert email here]>
2024-07-30 23:30:35 -0500

    add ollama rtk api to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,12 +9,14 @@ import appReducer from './appSlice';
 import themeReducer from '../theme/themeSlice';
 import { serverApi } from "../services/server/serverApi";
 import { openaiApi } from "../services/openai/openaiApi";
+import {ollamaApi} from "../services/ollama/ollamaApi";
 import {setupListeners} from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
     [serverApi.reducerPath]: serverApi.reducer,
     [openaiApi.reducerPath]: openaiApi.reducer,
+    [ollamaApi.reducerPath]: ollamaApi.reducer,
     actorDrawer: actorDrawerReducer,
     conversation: conversationReducer,
     messageCard: messageCardReducer,
@@ -24,7 +26,7 @@ export const store = configureStore({
     actorCreationDrawer: actorCreationDrawerReducer,
     app: appReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(serverApi.middleware, openaiApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(serverApi.middleware, openaiApi.middleware, ollamaApi.middleware),
 });
 
 setupListeners(store.dispatch);
