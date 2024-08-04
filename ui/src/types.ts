3e74e9a8578baa1982777572950eb99c7559a7b7
Greg Warner <[insert email here]>
2024-08-04 00:06:56 -0500

    added the dialog entity return type interface

--- ui/src/types.ts
+++ ui/src/types.ts
@@ -8,3 +8,12 @@ export interface ChatSnackbar {
     duration?: number,
     message: string,
 }
+
+export interface Dialog {
+    dialogId: number;
+    dialogName: string;
+    userId: number;
+    actorId: number;
+    timeCreated: number;
+    isDeleted: boolean;
+}
