34f95fbe2c50c1601c886dd7f70458a0c288a1c8
Greg Warner <[insert email here]>
2024-08-04 00:06:36 -0500

    added dialog response/request to server api types

--- ui/src/services/server/types.ts
+++ ui/src/services/server/types.ts
@@ -1,19 +1,34 @@
+import {Dialog} from "../../types";
+
 export interface ActorPrompt {
-    actorId: number,
-    prompt: string,
+    actorId: number;
+    prompt: string;
 }
 
 export interface ServerResponse {
-    msg: string,
+    msg: string;
 }
 
 export interface UpdateActorInput {
-    name: string,
-    title: string,
-    messageCard: string,
-    prompt: string,
-    avatar: File|string,
-    ttsModel: string,
-    chatModel: string,
-    actorId: number,
+    name: string;
+    title: string;
+    messageCard: string;
+    prompt: string;
+    avatar: File|string;
+    ttsModel: string;
+    chatModel: string;
+    actorId: number;
+}
+
+export interface DialogsResponse {
+    dialogs: Dialog[];
+}
+
+export interface DialogsRequest {
+    userId: number;
+    actorId: number;
+}
+
+export interface CreateDialogResponse {
+    dialogId: number;
 }
