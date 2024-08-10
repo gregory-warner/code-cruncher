95f5de02d8d4ec5f6afde904da4566cb7880ad62
Greg Warner <[insert email here]>
2024-08-09 22:15:56 -0500

    removed unused functions

--- ui/src/api/server/index.ts
+++ ui/src/api/server/index.ts
@@ -1,28 +1,6 @@
 import { AxiosResponse } from 'axios';
 import { apiClient } from './utils/apiClient';
 
-/* Users */
-export const getActiveUser = async (username: string): Promise<AxiosResponse<User>> => {
-    return await apiClient.get(`users/getUser/${username}`);
-};
-
-export const fetchActiveAssistants = async () => {
-    return await apiClient.get("actors/getActiveAssistants");
-};
-
-/* Actor Configuration */
-export const updateChatModel = async (actorChatModel: ActorModel): Promise<void> => {
-    await apiClient.patch(`actorConfigurations/updateChatModel`, actorChatModel);
-}
-
-export const getConfig = async (actorId: number): Promise<AxiosResponse<ActorConfiguration>> => {
-    return await apiClient.get(`actorConfigurations/getConfig/${actorId}`);
-}
-
-export const updateTtsModel = async (actorModel: ActorModel): Promise<void> => {
-    await apiClient.patch(`actorConfigurations/updateTtsModel`, actorModel);
-}
-
 /* Dialog */
 export const getDialogId = async (actorId: number, userId: number): Promise<AxiosResponse<number>> => {
     return await apiClient.get(`dialogs/getDialogId`, {
@@ -32,14 +10,5 @@ export const getDialogId = async (actorId: number, userId: number): Promise<Axio
 
 export const createDialog = async (actorId: number, userId: number): Promise<AxiosResponse<{dialogId: number}>> => {
     return await apiClient.post(`dialogs/createDialog`, {actorId, userId});
-};
-
-export const deleteDialog = async (dialogId: number): Promise<void> => {
-    await apiClient.patch(`dialogs/deleteDialog/${dialogId}`)
-};
-
-/* Message */
-export const getMessagesByDialogId = async (dialogId: number): Promise<AxiosResponse<Message[]>> => {
-    return await apiClient.get(`messages/getMessages/${dialogId}`);
 };
 
