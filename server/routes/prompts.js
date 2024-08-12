36dba32d9631bbd6deafdbdc6ecb76cd10b4817e
Greg Warner <[insert email here]>
2024-08-11 23:52:22 -0500

    commented pending wip code

--- server/routes/prompts.js
+++ server/routes/prompts.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import {Actor, ActorConfiguration} from "../models/models.js";
 import { Prompt } from "../models/models.js";
 
 const router = express.Router();
@@ -12,31 +11,32 @@ const deletePrompts = async (actorConfigurationId) => {
     });
 };
 
+// todo: rework
 router.post("/updatePrompt", async (req, res, next) => {
     const { actorId, prompt } = req.body;
 
-    const actor = await ActorConfiguration.findOne({
-        attributes: [["actor_configuration_id", "actorConfigurationId"]],
-        where: {
-            actor_id: actorId,
-        }
-    });
-
-    if (actor === null) {
-        const error = new Error(`Actor Configuration not found for actor id ${actorId}`);
-        error.status = 404;
-        return next(error);
-    }
-
-    const { actorConfigurationId } = actor.dataValues;
-
-    await deletePrompts(actorConfigurationId);
-    await Prompt.create({ actor_configuration_id: actorConfigurationId, prompt });
-
-    res.json({
-        "result": 200,
-        "message": "Actor prompt has been successfully updated.",
-    });
+    // const actor = await ActorConfiguration.findOne({
+    //     attributes: [["actor_configuration_id", "actorConfigurationId"]],
+    //     where: {
+    //         actor_id: actorId,
+    //     }
+    // });
+    //
+    // if (actor === null) {
+    //     const error = new Error(`Actor Configuration not found for actor id ${actorId}`);
+    //     error.status = 404;
+    //     return next(error);
+    // }
+    //
+    // const { actorConfigurationId } = actor.dataValues;
+    //
+    // await deletePrompts(actorConfigurationId);
+    // await Prompt.create({ actor_configuration_id: actorConfigurationId, prompt });
+    //
+    // res.json({
+    //     "result": 200,
+    //     "message": "Actor prompt has been successfully updated.",
+    // });
 });
 
 export default router;
