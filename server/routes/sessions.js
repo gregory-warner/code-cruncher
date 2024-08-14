542e6b5f3e2b073d73a5abad76aca8b8b2bfac00
Greg Warner <[insert email here]>
2024-08-14 00:06:14 -0500

    add session name and type id update endpoints

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import {createSession, getSessionById} from '../services/sessionService.js';
+import {createSession, getSessionById, updateSessionName, updateSessionTypeId} from '../services/sessionService.js';
 import {addSessionParticipant, getSessionParticipants} from "../services/sessionParticipantService.js";
 
 router.post('/create', async (req, res, next) => {
@@ -34,6 +34,24 @@ router.post('/add-participant', async (req, res, next) => {
     try {
         const participant = await addSessionParticipant(req.body);
         return res.json(participant);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.patch('/:id/name', async (req, res, next) => {
+    try {
+        const session = await updateSessionName({ sessionId: req.params.id, sessionName: req.body.sessionName });
+        return res.json(session);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.patch('/:id/typeId', async (req, res, next) => {
+    try {
+        const session = await updateSessionTypeId({ sessionId: req.params.id, sessionTypeId: req.body.sessionTypeId });
+        return res.json(session);
     } catch (err) {
         next(err);
     }
--- server/services/sessionService.js
+++ server/services/sessionService.js
@@ -23,3 +23,23 @@ export const createSession = async (sessionData) => {
 
     return session;
 }
+
+export const updateSessionName = async ({ sessionId, sessionName }) => {
+    const session = await Session.findByPk(sessionId);
+    if (!session) {
+        throw new Error(`No session found with ID ${sessionId}`);
+    }
+    session.sessionName = sessionName;
+    await session.save();
+    return session;
+}
+
+export const updateSessionTypeId = async ({ sessionId, sessionTypeId }) => {
+    const session = await Session.findByPk(sessionId);
+    if (!session instanceof Session) {
+        throw new Error(`No session found with ID ${sessionId}`);
+    }
+    session.sessionTypeId = sessionTypeId;
+    await session.save();
+    return session;
+};
