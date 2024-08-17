0d5c605db2a23e0c913950e7b2459a7b00681efb
Greg Warner <[insert email here]>
2024-08-17 00:22:04 -0500

    add prompt service; update prompts route

--- server/routes/prompts.js
+++ server/routes/prompts.js
@@ -1,42 +1,23 @@
 import express from 'express';
-import { Prompt } from "../models/models.js";
 
 const router = express.Router();
 
-const deletePrompts = async (actorConfigurationId) => {
-    await Prompt.update({is_deleted: true}, {
-        where: {
-            actor_configuration_id: actorConfigurationId,
-        }
-    });
-};
+router.post("/", async (req, res, next) => {
+    try {
+        const newPrompt = await createPrompt(req.body);
+        return res.json(newPrompt);
+    } catch (err) {
+        next(err);
+    }
+});
 
-// todo: rework
-router.post("/updatePrompt", async (req, res, next) => {
-    const { actorId, prompt } = req.body;
-
-    // const actor = await ActorConfiguration.findOne({
-    //     attributes: [["actor_configuration_id", "actorConfigurationId"]],
-    //     where: {
-    //         actor_id: actorId,
-    //     }
-    // });
-    //
-    // if (actor === null) {
-    //     const error = new Error(`Actor Configuration not found for actor id ${actorId}`);
-    //     error.status = 404;
-    //     return next(error);
-    // }
-    //
-    // const { actorConfigurationId } = actor.dataValues;
-    //
-    // await deletePrompts(actorConfigurationId);
-    // await Prompt.create({ actor_configuration_id: actorConfigurationId, prompt });
-    //
-    // res.json({
-    //     "result": 200,
-    //     "message": "Actor prompt has been successfully updated.",
-    // });
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const deletedPrompt = await deletePrompt(req.params.id);
+        return res.json(deletedPrompt);
+    } catch (err) {
+        next(err);
+    }
 });
 
 export default router;
