2924af979213bd93c73b621329fe3e9308508d40
Greg Warner <>
2024-08-18 22:58:46 -0500

    set center text of header to current session speaker

--- ui/src/features/chatApp/components/header/ChatAppHeader.tsx
+++ ui/src/features/chatApp/components/header/ChatAppHeader.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import {Box, Grid, Typography, useTheme} from '@mui/material';
 import { useAppSelector } from '../../../../store/hooks';
-import {selectActor} from "../../../conversation/store/conversationSlice";
+import {selectCurrentSpeaker} from "../../../session/store/sessionSlice";
 import ChatAppMenu from "../chatAppMenu/ChatAppMenu";
 
 const ChatAppHeader: React.FC = () => {
-    const assistant = useAppSelector(selectActor);
+    const currentSpeaker = useAppSelector(selectCurrentSpeaker);
     const theme = useTheme();
+
+    const speakerName = currentSpeaker?.name ?? '';
 
     return (
         <Box sx={{maxHeight: '8vh'}} display={"flex"} justifyContent={"center"}>
             <Grid container justifyContent={"space-between"} alignItems={"center"} bgcolor="rgba(34, 34, 34, 0.9)">
                 <Grid item xs={2} sx={{flexGrow: 1}}></Grid>
                 <Grid item xs={8}>
-                    <Typography align='center' color={theme.palette.text.primary} variant={"h4"}>{assistant?.name ?? ""}</Typography>
+                    <Typography align='center' color={theme.palette.text.primary} variant={"h4"}>{speakerName}</Typography>
                 </Grid>
                 <Grid item xs={2}>
                     <ChatAppMenu />
