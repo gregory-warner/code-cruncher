b086355f593dbc748e876c06fa1d746c2a9a76e7
Greg Warner <>
2024-08-19 22:22:09 -0500

    get default user when app loads

--- ui/src/features/codeCruncher/CodeCruncherApp.tsx
+++ ui/src/features/codeCruncher/CodeCruncherApp.tsx
@@ -1,11 +1,32 @@
-import Session from '../session/Session';
-import { Box, Grid } from '@mui/material';
-import UserInput from './components/userInput/UserInput';
+import {Box, CircularProgress, Grid} from '@mui/material';
 import AppHeader from './components/header/AppHeader';
 import style from './styles';
 import SessionsSection from "../sessionsSection/SessionsSection";
+import {useGetUserQuery} from "../../services/server/serverApi";
+import React, {useEffect} from "react";
+import {useAppDispatch} from "../../store/hooks";
+import {defaultUserId, setUser} from "../user/userSlice";
+import {isUser} from "../../utils/util";
 
 const CodeCruncherApp = () => {
+    const dispatch = useAppDispatch();
+
+    const { data: userData, isLoading: isUserDataLoading } = useGetUserQuery(defaultUserId);
+
+    useEffect(() => {
+        if (isUser(userData)) {
+            dispatch(setUser(userData));
+        }
+    }, [userData]);
+
+    if (isUserDataLoading) {
+        return (
+            <CircularProgress
+                aria-label="Loading user..."
+            />
+        );
+    }
+
     return (
         <Grid container direction="column" sx={{ overflowY: "hidden",}}>
             <Grid item xs={12} sx={style.header}>
