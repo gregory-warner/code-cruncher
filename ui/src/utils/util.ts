f0100b70447d4ab9b116e152a0379aea5de0f444
Greg Warner <>
2024-08-19 22:21:34 -0500

    add isUser util

--- ui/src/utils/util.ts
+++ ui/src/utils/util.ts
@@ -1,6 +1,6 @@
 import {Model} from "../services/ollama/types";
 import {ChatApiModel} from "../services/openai/types";
-import {Actor} from "../types";
+import {Actor, User} from "../types";
 
 export const getTimestamp = () => Math.floor(Date.now()/1000);
 
@@ -8,6 +8,7 @@ export const isFile = (file: unknown): file is File => file instanceof File;
 export const isActor = (actor: Actor | null): actor is Actor => actor && typeof actor !== 'undefined';
 export const isOpenAiModel = (model: Model|ChatApiModel) => 'object' in model;
 export const isOllamaModel = (model: Model|ChatApiModel) => 'digest' in model;
+export const isUser = (user: User | null): user is User => user && typeof user !== 'undefined';
 
 export const messengerTypeIds = {
     user: 0,
