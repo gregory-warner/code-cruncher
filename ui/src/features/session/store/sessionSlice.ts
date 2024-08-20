95503fd3baccd22281a1fdca864b6bc31c063766
Greg Warner <>
2024-08-19 22:20:33 -0500

    add addParticipant to session slice

--- ui/src/features/session/store/sessionSlice.ts
+++ ui/src/features/session/store/sessionSlice.ts
@@ -1,13 +1,13 @@
 import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {RootState} from '../../../store/store';
 import {createDialog, getDialogId} from '../../../api/server';
-import {Actor, Message} from "../../../types";
+import {Actor, Message, SessionParticipant, User} from "../../../types";
 
-export const defaultUser = "eagle-bonnet";
+
 
 export interface SessionState {
     sessionId: number|null,
-    participants: (User | Actor)[]
+    participants: SessionParticipant[]
     currentSpeaker: User | Actor | null,
 }
 
@@ -24,116 +24,114 @@ export const sessionSlice = createSlice({
         setSessionId: (state, action: PayloadAction<number>) => {
             state.sessionId = action.payload;
         },
-        setParticipants: (state, action: PayloadAction<any>) => {
-
+        setParticipants: (state, action: PayloadAction<SessionParticipant[]>) => {
+            state.participants = action.payload;
         },
+        addSessionParticipant: (state, action: PayloadAction<SessionParticipant>) => {
+            state.participants = [
+                ...state.participants,
+                action.payload,
+            ];
+        }
     },
-    extraReducers: builder => {
-        builder.addCase(updateDialogId.fulfilled, (state, action) => {
-            // state.dialogId = action.payload;
-        });
-    }
 });
 
 export const {
     setSessionId,
-    setParticipants
+    setParticipants,
+    addSessionParticipant,
 } = sessionSlice.actions;
 
 export const selectSessionId = (state: RootState): number => state.session.sessionId;
-export const selectParticipants = (state: RootState): (User | Actor)[] => state.session.participants;
+export const selectParticipants = (state: RootState): SessionParticipant[] => state.session.participants;
 export const selectCurrentSpeaker = (state: RootState): User | Actor | null => state.session.currentSpeaker;
 
 
-
-
-
-
 /* Message */
-export const getChatMessages = (messages: Message[], assistant: Actor): ChatMessage[] => {
-    // const prompt: ChatMessage = {
-    //     role: "system",
-    //     content: assistant.configuration.prompt ?? '',
-    // };
-    // let chatMessages: ChatMessage[] = [prompt];
-    //
-    // for (const message of messages) {
-    //     chatMessages.push({
-    //         content: message.content,
-    //         role: messengerTypes[message.messengerTypeId],
-    //     } as ChatMessage);
-    // }
-
-    // return chatMessages;
-};
-
-export const sendChatMessage = createAsyncThunk<void, Message>(
-    "message/sendChatMessage",
-    async (message: Message, { dispatch, getState }) => {
-
-        // const state = getState() as RootState;
-        //
-        // const dialogId = message.dialogId;
-        // const assistant = selectActor(state);
-        //
-        // const messages = await dispatch(serverApi.endpoints.getMessages.initiate(dialogId)).unwrap();
-        // const chatMessages = getChatMessages([...messages, message], assistant);
-        //
-        // await dispatch(serverApi.endpoints.addMessage.initiate(message)).unwrap();
-        //
-        // const chatResponse = await dispatch(ollamaApi.endpoints.chat.initiate({
-        //     model: 'codegeex-v2:latest',
-        //     messages: [{role: 'system', content: 'speak like a pirate'}, {role: 'user', content: 'what are the first 5 digits of PI?'}],
-        //     stream: false,
-        // })).unwrap();
-        //
-        // // const chatResponse: ChatMessage = await sendChatRequest({chatMessages, chatModel: assistant.configuration?.chatModel});
-        //
-        // // if (assistant.configuration.ttsModel) {
-        // //     sayText({text: chatResponse.content, model: assistant.configuration.ttsModel});
-        // // }
-        //
-        // const messageResponse: Message = {
-        //     messengerId: assistant.actorId,
-        //     // messengerTypeId: messengerTypeIds.assistant,
-        //     // dialogId: dialogId,
-        //     content: chatResponse.message.content,
-        //     // timestamp: getTimestamp(),
-        // };
-        //
-        // await dispatch(serverApi.endpoints.addMessage.initiate(messageResponse)).unwrap();
-    }
-);
+// export const getChatMessages = (messages: Message[], assistant: Actor): ChatMessage[] => {
+//     // const prompt: ChatMessage = {
+//     //     role: "system",
+//     //     content: assistant.configuration.prompt ?? '',
+//     // };
+//     // let chatMessages: ChatMessage[] = [prompt];
+//     //
+//     // for (const message of messages) {
+//     //     chatMessages.push({
+//     //         content: message.content,
+//     //         role: messengerTypes[message.messengerTypeId],
+//     //     } as ChatMessage);
+//     // }
+//
+//     // return chatMessages;
+// };
+//
+// export const sendChatMessage = createAsyncThunk<void, Message>(
+//     "message/sendChatMessage",
+//     async (message: Message, { dispatch, getState }) => {
+//
+//         // const state = getState() as RootState;
+//         //
+//         // const dialogId = message.dialogId;
+//         // const assistant = selectActor(state);
+//         //
+//         // const messages = await dispatch(serverApi.endpoints.getMessages.initiate(dialogId)).unwrap();
+//         // const chatMessages = getChatMessages([...messages, message], assistant);
+//         //
+//         // await dispatch(serverApi.endpoints.addMessage.initiate(message)).unwrap();
+//         //
+//         // const chatResponse = await dispatch(ollamaApi.endpoints.chat.initiate({
+//         //     model: 'codegeex-v2:latest',
+//         //     messages: [{role: 'system', content: 'speak like a pirate'}, {role: 'user', content: 'what are the first 5 digits of PI?'}],
+//         //     stream: false,
+//         // })).unwrap();
+//         //
+//         // // const chatResponse: ChatMessage = await sendChatRequest({chatMessages, chatModel: assistant.configuration?.chatModel});
+//         //
+//         // // if (assistant.configuration.ttsModel) {
+//         // //     sayText({text: chatResponse.content, model: assistant.configuration.ttsModel});
+//         // // }
+//         //
+//         // const messageResponse: Message = {
+//         //     messengerId: assistant.actorId,
+//         //     // messengerTypeId: messengerTypeIds.assistant,
+//         //     // dialogId: dialogId,
+//         //     content: chatResponse.message.content,
+//         //     // timestamp: getTimestamp(),
+//         // };
+//         //
+//         // await dispatch(serverApi.endpoints.addMessage.initiate(messageResponse)).unwrap();
+//     }
+// );
 
 /* Dialog */
 
-interface UpdateDialogPayload {
-    user: User;
-    actor: Actor;
-}
-
-export const updateDialogId = createAsyncThunk<number, UpdateDialogPayload>(
-    'dialog/updateDialogId',
-    async (payload: UpdateDialogPayload) => {
-        const { user, actor } = payload;
-        return await getCurrentDialogId(actor.actorId, user.userId);
-    });
-
-const getCurrentDialogId = async (actorId: number, userId: number): Promise<number> => {
-    if (actorId <= 0 || userId <= 0) {
-        console.log('Unable to get dialog ID: ', actorId, ': ', userId);
-        return -1;
-    }
-    const currentDialog = await getDialogId(actorId, userId);
-    let currentDialogId = currentDialog.data ?? -1;
-
-    if (currentDialogId > 0) {
-        return currentDialogId;
-    }
-
-    const dialog = await createDialog(actorId, userId);
-    return dialog.data.dialogId ?? -1;
-};
+// interface UpdateDialogPayload {
+//     user: User;
+//     actor: Actor;
+// }
+//
+// export const updateDialogId = createAsyncThunk<number, UpdateDialogPayload>(
+//     'dialog/updateDialogId',
+//     async (payload: UpdateDialogPayload) => {
+//         const { user, actor } = payload;
+//         return await getCurrentDialogId(actor.actorId, user.userId);
+//     });
+//
+// const getCurrentDialogId = async (actorId: number, userId: number): Promise<number> => {
+//     if (actorId <= 0 || userId <= 0) {
+//         console.log('Unable to get dialog ID: ', actorId, ': ', userId);
+//         return -1;
+//     }
+//     const currentDialog = await getDialogId(actorId, userId);
+//     let currentDialogId = currentDialog.data ?? -1;
+//
+//     if (currentDialogId > 0) {
+//         return currentDialogId;
+//     }
+//
+//     const dialog = await createDialog(actorId, userId);
+//     return dialog.data.dialogId ?? -1;
+// };
 
 // export const selectDialogId = (state: RootState): number => state.session.dialogId;
 // export const selectActor = (state: RootState): Actor => state.session.actor;
