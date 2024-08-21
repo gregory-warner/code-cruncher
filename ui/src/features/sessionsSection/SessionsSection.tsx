3a16f779f64f1d9a3b456396a48f30b85238d34c
Greg Warner <>
2024-08-20 22:53:11 -0500

    fix sessionsection style

--- ui/src/features/sessionsSection/SessionsSection.tsx
+++ ui/src/features/sessionsSection/SessionsSection.tsx
@@ -3,28 +3,27 @@ import {
     useAddParticipantMutation,
     useCreateSessionMutation,
     useGetSessionsQuery
-} from "../../services/server/serverApi";
-import SessionItem from "./components/SessionItem";
-import React from "react";
-import {SessionRequest} from "../../services/server/types";
-import {Session, SessionParticipantType, SessionType, User} from "../../types";
-import {useAppDispatch, useAppSelector} from "../../store/hooks";
-import {selectUser} from "../user/userSlice";
-import {addSessionParticipant, setSessionId} from "../session/store/sessionSlice";
-import {isUser} from "../../utils/util";
+} from '../../services/server/serverApi';
+import SessionItem from './components/SessionItem';
+import React from 'react';
+import {SessionRequest} from '../../services/server/types';
+import {Session, SessionType, User} from '../../types';
+import {useAppDispatch, useAppSelector} from '../../store/hooks';
+import {selectUser} from '../user/userSlice';
+import {setSessionId} from '../session/store/sessionSlice';
+import {isUser} from '../../utils/util';
 
 const SessionsSection = () => {
     const dispatch = useAppDispatch();
     const user: User = useAppSelector(selectUser);
 
     const [createSession] = useCreateSessionMutation();
-    const [addParticipant] = useAddParticipantMutation();
     const {data, isLoading} = useGetSessionsQuery();
 
     if (isLoading || !isUser(user)) {
         return (
             <CircularProgress
-                aria-label="Loading sessions..."
+                aria-label='Loading sessions...'
             />
         );
     }
@@ -40,30 +39,33 @@ const SessionsSection = () => {
     };
 
     return (
-        <Stack sx={{border: '1px solid black'}}>
-            <Grid container alignItems="center" spacing={2}>
-                <Grid item xs>
-                    <Typography variant='h6' gutterBottom sx={{padding: '5px'}}>
+        <Stack sx={{ height: '100%' }}>
+            <Grid container direction='row' alignItems='center'>
+                <Grid item xs={5} textAlign='center'>
+                    <Typography variant='h6' sx={{paddingLeft: '5px', paddingRight: '5px'}}>
                         Sessions
                     </Typography>
                 </Grid>
-                <Grid item xs={7}>
-                    <Button sx={{ width: '100%' }} onClick={createNewSession}>+ New Session</Button>
+                <Grid item xs={7} textAlign='center'>
+                    <Button
+                        sx={{ width: '100%', textTransform: 'none', height: '100%' }}
+                        onClick={createNewSession}>
+                        <Typography variant='body2'>+ New Session</Typography>
+                    </Button>
                 </Grid>
             </Grid>
-            <List>
-                {data.map((session, idx) => (
-                    <Grid key={`key-session-item-${idx}`} container justifyContent="center" alignItems="center">
-                        <SessionItem
-                            key={`key-session-item-${idx}`}
-                            sessionId={idx}
-                            session={session}
-                        />
-                    </Grid>
+            <List sx={{ overflowY: 'auto', height: 'calc(100% - 56px)', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                {[...data].reverse().map((session, idx) => (
+                    <SessionItem
+                        key={`key-session-item-${idx}`}
+                        sessionId={idx}
+                        session={session}
+                    />
                 ))}
             </List>
         </Stack>
     );
+
 };
 
 export default SessionsSection;
