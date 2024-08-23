be5a3439c24deda06c48246d8d1e9780da2cb4e0
Greg Warner <>
2024-08-23 00:40:11 -0500

    update user input with current db changes

--- ui/src/features/session/component/userInput/UserInput.tsx
+++ ui/src/features/session/component/userInput/UserInput.tsx
@@ -1,38 +1,56 @@
 import React, { useState, useCallback } from 'react';
 import { Button, Grid, TextField } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
-import { useAppDispatch, useAppSelector } from '../../../../store/hooks';
-import {
-    selectDialogId,
-    selectUser,
-    sendChatMessage
-} from '../../../session/store/sessionSlice';
-import {messengerTypeIds} from "../../../../utils/util";
+import {useAppDispatch, useAppSelector} from '../../../../store/hooks';
+import {incrementCurrentSequenceId, selectCurrentSpeaker, selectSessionId} from '../../store/sessionSlice';
+import {selectUser} from "../../../user/userSlice";
+import {useAddMessageMutation} from "../../../../services/server/serverApi";
+import {MessageRequest} from "../../../../services/server/types";
+import {MessageTypeId} from "../../../../types";
+import {setSnackbar} from "../../../../app/store/appSlice";
 
 const UserInput = () => {
     const dispatch = useAppDispatch();
     const user = useAppSelector(selectUser);
-    const dialogId = useAppSelector(selectDialogId);
+    const currentSpeaker = useAppSelector(selectCurrentSpeaker);
+    const sessionId = useAppSelector(selectSessionId);
 
-    const [input, setInput] = useState("");
+    const [addMessage] = useAddMessageMutation();
+
+    const isUserCurrentSpeaker = (
+        user &&
+        currentSpeaker &&
+        'userId' in currentSpeaker &&
+        'userId' in user &&
+        user.userId === currentSpeaker.userId
+    );
+
+    const [input, setInput] = useState('');
 
     const onInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
         setInput(event.target.value);
     }, []);
 
     const onSend = async () => {
-        if (!input.trim() || dialogId <= 0 || user.userId <= 0) {
+        if (!input.trim() || sessionId <= 0 || user.userId <= 0) {
             return;
         }
 
-        const message: Message = {
-            dialogId: dialogId,
+        const message: MessageRequest = {
+            sessionId,
             messengerId: user.userId,
-            messengerTypeId: messengerTypeIds['user'] ?? 0,
+            messageTypeId: MessageTypeId.general,
             content: input,
         };
 
-        dispatch(sendChatMessage(message));
+        const newMessage = await addMessage(message).unwrap();
+
+        if (!newMessage.messageId) {
+            dispatch(setSnackbar({message: `Unable to send message`}));
+            return;
+        }
+
+        dispatch(incrementCurrentSequenceId());
 
         setInput("");
     };
@@ -43,7 +61,7 @@ const UserInput = () => {
                 event.preventDefault();
                 return;
             }
-            onSend();
+            onSend().catch(_ => dispatch(setSnackbar({message: `Unable to send message`})));
         }
     }
 
@@ -58,9 +76,10 @@ const UserInput = () => {
                     multiline
                     fullWidth
                     value={input}
-                    minRows={14}
+                    minRows={12}
                     onChange={onInputChange}
                     onKeyUp={handleKeyUp}
+                    disabled={!isUserCurrentSpeaker}
                 />                
             </Grid>
             <Grid container alignItems={'center'} spacing={2}>
@@ -78,7 +97,6 @@ const UserInput = () => {
                     </Button>
                 </Grid>
             </Grid>
-
         </Grid>
     );
 };
