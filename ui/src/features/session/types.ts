1625f3d29ae5ce76e88ea1b71544a75594c59da6
Greg Warner <>
2024-08-24 23:31:05 -0500

    add chat service interface

---
+++ ui/src/features/session/types.ts
@@ -0,0 +1,6 @@
+import {Message} from "../../types";
+import {AnyAction, ThunkAction} from "@reduxjs/toolkit";
+
+export interface ChatService {
+    chat(messages: Message[]): ThunkAction<any, any, any, AnyAction>;
+}
