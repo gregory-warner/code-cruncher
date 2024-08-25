fb0f67d1df32b23f24d14ca0e25c31e172fb7011
Greg Warner <>
2024-08-24 23:27:28 -0500

    remove unused api endpoint

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import {createMessage, deleteMessage, getMessages, getMessenger, updateMessage} from '../services/messagesService.js';
+import {createMessage, deleteMessage, getMessages, updateMessage} from '../services/messagesService.js';
 
 router.get('/:sessionId', async (req, res, next) => {
     try {
@@ -17,17 +17,6 @@ router.post('/create', async (req, res, next) => {
         return res.json(message.get({plain: true}));
     } catch (error) {
         next(`Unable to add message: ${error}`);
-    }
-});
-
-router.get('/get-messenger/:messengerId', async (req, res, next) => {
-    try {
-        const messengerId = parseInt(req.params.messengerId) ?? 0;
-        const messengerTypeId = parseInt(req.query.messengerTypeId) ?? 0;
-        const messenger = await getMessenger(messengerId, messengerTypeId);
-        return res.json(messenger);
-    } catch (err) {
-        next(err);
     }
 });
 
