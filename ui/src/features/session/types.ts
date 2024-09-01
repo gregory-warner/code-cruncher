db6bb179b69dca68c19a005e0e7344eabcc734a8
Greg Warner <cats-carrying-canes@canary.net>
2024-08-31 23:49:05 -0500

    add messenger type

--- ui/src/features/session/types.ts
+++ ui/src/features/session/types.ts
@@ -1,7 +1,9 @@
-import {Message} from "../../types";
+import {Actor, Message, User} from "../../types";
 import {AnyAction, ThunkAction} from "@reduxjs/toolkit";
 
 export interface ChatService {
     chat(messages: Message[]): ThunkAction<any, any, any, AnyAction>;
     getMessageResponse: any;
 }
+
+export type Messenger = User | Actor;
