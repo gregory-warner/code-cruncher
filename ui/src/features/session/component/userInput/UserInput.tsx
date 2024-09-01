394942e69d1d323acd3ee49d95d4b819cbdabe52
Greg Warner <cats-carrying-canes@canary.net>
2024-08-31 23:49:12 -0500

    fix user input style

--- ui/src/features/session/component/userInput/UserInput.tsx
+++ ui/src/features/session/component/userInput/UserInput.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import { Button, Grid, TextField } from '@mui/material';
+import {Box, Button, Grid, TextField} from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import {useAppDispatch, useAppSelector} from '../../../../store/hooks';
 import {incrementCurrentSequenceId, selectCurrentSpeaker, selectSessionId} from '../../store/sessionSlice';
@@ -51,7 +51,7 @@ const UserInput = () => {
             return;
         }
 
-        dispatch(incrementCurrentSequenceId());
+        // dispatch(incrementCurrentSequenceId());
 
         setInput("");
     };
@@ -67,38 +67,30 @@ const UserInput = () => {
     }
 
     return(
-        <Grid direction={"column"} container alignContent={"center"} alignItems={"stretch"} justifyContent={"space-between"} sx={{height: '100%', border: '1px solid black'}}>
-            <Grid item xs={11}  borderRight={4} borderColor={"rgba(34, 34, 34, 1)"} color={"rgba(34, 34, 34, 1)"} sx={{width: '100%'}}>
-                <TextField 
-                    id="id-user-input" 
-                    label="Message"
-                    variant="filled" 
-                    color="primary"
-                    multiline
-                    fullWidth
-                    value={input}
-                    minRows={12}
-                    onChange={onInputChange}
-                    onKeyUp={handleKeyUp}
-                    disabled={!isUserCurrentSpeaker}
-                />                
-            </Grid>
-            <Grid container alignItems={'center'} spacing={2}>
-                <Grid item xs={3}></Grid>
-                <Grid item xs={3}></Grid>
-                <Grid item xs={3}></Grid>
-                <Grid container item xs={3} textAlign={'right'} alignItems={'center'} justifyContent={'flex-end'} padding={0.1}>
-                    <Button
-                        variant="contained"
-                        size="small"
-                        endIcon={<SendIcon />}
-                        onClick={onSend}
-                    >
-                        Send
-                    </Button>
-                </Grid>
-            </Grid>
-        </Grid>
+        <Box>
+            <TextField
+                id="id-user-input"
+                label="Message"
+                variant="filled"
+                color="primary"
+                multiline
+                fullWidth
+                value={input}
+                rows={9}
+                onChange={onInputChange}
+                onKeyUp={handleKeyUp}
+                disabled={!isUserCurrentSpeaker}
+                sx={{ flexGrow: 1 }}
+            />
+            <Button
+                variant="contained"
+                size="small"
+                endIcon={<SendIcon />}
+                onClick={onSend}
+            >
+                Send
+            </Button>
+        </Box>
     );
 };
 
