f9949e662e80713eedac349c7281f3a6894abcc1
Greg Warner <cats-carrying-canes@canary.net>
2024-09-02 22:08:03 -0500

    init session message image section

---
+++ ui/src/features/session/component/sessionMessage/components/SessionMessageImageSection.tsx
@@ -0,0 +1,9 @@
+import {FunctionComponent} from "react";
+
+const SessionMessageImageSection: FunctionComponent = () => {
+    return (
+        <></>
+    );
+};
+
+export default SessionMessageImageSection;
