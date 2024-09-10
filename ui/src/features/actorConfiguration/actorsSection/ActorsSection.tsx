b3de1a6b7c71cc8f8de87ccda4739e3fb8087700
Greg Warner <cats-carrying-canes@canary.net>
2024-09-09 22:37:43 -0500

    add actors configuration section

---
+++ ui/src/features/actorConfiguration/actorsSection/ActorsSection.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Box, Button, Grid, List, Stack, Typography} from '@mui/material';
+import ActorItem from "./components/ActorItem";
+import {useGetActorsQuery} from "../../../services/server/serverApi";
+
+const ActorsSection = () => {
+
+    const { data: actors } = useGetActorsQuery();
+
+    if (!actors) {
+        return <Box />;
+    }
+
+    return (
+        <Stack sx={{ height: '100%' }}>
+            <Grid container direction='row' alignItems='center'>
+                <Grid item xs={5} textAlign='center'>
+                    <Typography variant='h6' sx={{paddingLeft: '5px', paddingRight: '5px'}}>
+                        Actors
+                    </Typography>
+                </Grid>
+                <Grid item xs={7} textAlign='center'>
+                    <Button
+                        sx={{ width: '100%', textTransform: 'none', height: '100%' }}
+                        onClick={() => {}}>
+                        <Typography variant='body2'>+ New Actor</Typography>
+                    </Button>
+                </Grid>
+            </Grid>
+            <List sx={{ overflowY: 'auto', height: 'calc(100% - 56px)', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                {actors.map((actor, idx) => (
+                    <ActorItem
+                        key={`key-actor-item-${idx}`}
+                        itemId={idx}
+                        actor={actor}
+                    />
+                ))}
+            </List>
+        </Stack>
+    );
+
+};
+
+export default ActorsSection;
---
+++ ui/src/features/actorConfiguration/actorsSection/components/ActorItem.tsx
@@ -0,0 +1,75 @@
+import React, {useState} from "react";
+import {Grid, ListItem, TextField, Typography, useTheme} from "@mui/material";
+import {useAppDispatch} from "../../../../store/hooks";
+import {setSelectedActor} from "../../store/actorConfigurationSlice";
+
+interface ActorItemParams {
+    actor: Actor;
+    itemId: number;
+}
+
+const ActorItem = ({ actor, itemId }: ActorItemParams) => {
+    const theme = useTheme();
+
+    const dispatch = useAppDispatch();
+
+    const [editMode, setEditMode] = useState(false);
+    const [editedSessionName, setEditedSessionName] = useState<string>(actor.name);
+
+    const updateSelectedActor = () => {
+        dispatch(setSelectedActor(actor));
+    };
+
+    return (
+        <ListItem
+            onClick={updateSelectedActor}
+            sx={{
+                width: '95%',
+                textTransform: 'none',
+                color: 'inherit',
+                '&:hover': { cursor: 'pointer' },
+                '&:hover:before': {
+                    content: '""',
+                    position: 'absolute',
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0,
+                    outlineOffset: '-1px',
+                    transition: 'border-color .3s, box-shadow .3s',
+                    boxShadow: `0 0 10px ${theme.palette.primary.main}`,
+                },
+            }}
+        >
+            <Grid container alignItems='center' sx={{ flexGrow: 1}}>
+                <Grid item xs={1}>
+                    <Typography variant='body1'>{`${itemId+1}.) `}</Typography>
+                </Grid>
+                <Grid item xs={7} sx={{ paddingLeft: '5px'}}>
+                    {
+                        editMode ? (
+                            <TextField
+                                value={editedSessionName}
+                                onChange={(e) => setEditedSessionName(e.target.value)}
+                                size="small"
+                            />
+                        ) : (
+                            <Typography
+                                whiteSpace='nowrap'
+                                overflow='hidden'
+                                textOverflow='ellipsis'
+                                paddingLeft='3px'
+                                align='left'
+                                variant='body1'
+                            >
+                                {actor.name}
+                            </Typography>
+                        )
+                    }
+                </Grid>
+            </Grid>
+        </ListItem>
+    );
+};
+
+export default ActorItem;
