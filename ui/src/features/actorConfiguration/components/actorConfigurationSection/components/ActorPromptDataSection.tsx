5a0e2eeb52d15d3aee257ebc9253c6a56524f1a1
Greg Warner <cats-carrying-canes@canary.net>
2024-09-10 23:53:47 -0500

    update prompt on save

--- ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorPromptDataSection.tsx
+++ ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorPromptDataSection.tsx
@@ -1,12 +1,30 @@
 import {ActorDisplayItem} from '../../../types';
 import ActorDataSection from './ActorDataSection';
 import React, {useState} from 'react';
-import {Prompt} from '../../../../../types';
+import {Actor, Prompt} from '../../../../../types';
 import {TextField} from "@mui/material";
+import {useAppSelector} from "../../../../../store/hooks";
+import {selectSelectedActor} from "../../../store/actorConfigurationSlice";
+import {useUpdatePromptMutation} from "../../../../../services/server/serverApi";
 
-const ActorPromptDataSection = ({ prompt }: {prompt: Prompt}) => {
+const ActorPromptDataSection = () => {
+    const actor: Actor|null = useAppSelector(selectSelectedActor)
+
+    const prompt = actor.prompt;
+
+    const [updatePrompt] = useUpdatePromptMutation();
 
     const [actorPrompt, setActorPrompt] = useState<string>(prompt.prompt);
+
+    const onSave = async () => {
+        setActorPrompt(actorPrompt);
+        const response = await updatePrompt({
+            actorId: parseInt(actor.actorId),
+            prompt: {
+                prompt: actorPrompt,
+            },
+        });
+    }
 
     const items: ActorDisplayItem[] = [
         {
@@ -27,7 +45,7 @@ const ActorPromptDataSection = ({ prompt }: {prompt: Prompt}) => {
     ];
 
     return (
-        <ActorDataSection items={items} title='Prompt' />
+        <ActorDataSection items={items} title='Prompt' onSave={onSave} />
     );
 };
 
