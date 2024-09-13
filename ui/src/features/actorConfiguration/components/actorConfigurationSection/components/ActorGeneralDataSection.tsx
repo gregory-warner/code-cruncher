e834bab81cd67d4d0d70fb31b2331bae0c58fad1
Greg Warner <cats-carrying-canes@canary.net>
2024-09-12 22:16:55 -0500

    update general actor data

--- ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorGeneralDataSection.tsx
+++ ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorGeneralDataSection.tsx
@@ -3,26 +3,41 @@ import ActorDataSection from './ActorDataSection';
 import React, {useState} from 'react';
 import {Actor} from '../../../../../types';
 import {TextField} from "@mui/material";
+import {useUpdateActorMutation} from "../../../../../services/server/serverApi";
 
 const ActorGeneralDataSection = ({ actor }: {actor: Actor}) => {
 
-    const [name, setName] = useState<string>(actor.name);
-    const [title, setTitle] = useState<string>(actor.title);
+    const [updatedActor, setUpdatedActor] = useState<Actor>(actor);
+
+    const [updateActor] = useUpdateActorMutation();
 
     const onSave = () => {
-
+        updateActor({
+            actorId: actor.actorId,
+            ...updatedActor,
+        });
     };
 
     const items: ActorDisplayItem[] = [
         {
             label: 'Name',
-            value: actor.name,
-            editComponent: <TextField defaultValue={name} onChange={(event) => setName(event.target.value)} />
+            value: updatedActor.name,
+            editComponent: (
+                <TextField
+                    defaultValue={updatedActor.name}
+                    onChange={(event) => setUpdatedActor({ ...updatedActor, name: event.target.value })}
+                />
+            )
         },
         {
             label: 'Title',
-            value: actor.title,
-            editComponent: <TextField defaultValue={title} onChange={(event) => setTitle(event.target.value)} />
+            value: updatedActor.title,
+            editComponent: (
+                <TextField
+                    defaultValue={updatedActor.title}
+                    onChange={(event) => setUpdatedActor({ ...updatedActor, title: event.target.value })}
+                />
+            )
         },
     ];
 
