9b509c63a5c2c577a274181ab6d1863fb0e57355
Greg Warner <cats-carrying-canes@canary.net>
2024-10-03 23:49:09 -0500

    add logic to update avatar with user click and selecting file

--- ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorAvatarDataSection.tsx
+++ ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorAvatarDataSection.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import {Avatar, Box, IconButton, Tooltip, Typography} from "@mui/material";
 import {chatServerUrl} from "../../../../../../config";
 import {Actor} from "../../../../../types";
+import {useUpdateActorMutation, useUpdateAvatarMutation} from "../../../../../services/server/serverApi";
 
 const ActorAvatarDataSection = ({ actor }: { actor: Actor }) => {
+    const [updatedActor, setUpdatedActor] = useState<Actor>(actor);
+
+    useEffect(() => {
+        setUpdatedActor(actor);
+    }, [actor]);
+
+    const fileInput = React.useRef<HTMLInputElement>();
+
+    const [updateAvatar] = useUpdateAvatarMutation();
 
     const onAvatarClick = () => {
+        if (fileInput?.current) {
+            fileInput.current.click();
+        }
+    };
 
+    const onFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+            const formData: FormData = new FormData();
+            formData.append('actorId', actor.actorId.toString());
+            formData.append('avatar', event.target.files[0]);
+            updateAvatar({
+                actorId: actor.actorId,
+                formData,
+            });
+        }
     };
 
     return (
@@ -24,6 +48,12 @@ const ActorAvatarDataSection = ({ actor }: { actor: Actor }) => {
             <Typography variant='h5' component='div'>
                 {actor.name}
             </Typography>
+            <input
+                ref={fileInput}
+                type='file'
+                style={{ display: 'none' }}
+                onChange={onFileChange}
+            />
         </Box>
     );
 };
