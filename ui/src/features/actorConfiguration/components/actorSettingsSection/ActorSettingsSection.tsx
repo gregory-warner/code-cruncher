4087d83006d2e1e23a5c69dd81d3fcd32b4bea6e
Greg Warner <cats-carrying-canes@canary.net>
2024-10-08 21:26:52 -0500

    add New action to config

--- ui/src/features/actorConfiguration/components/actorSettingsSection/ActorSettingsSection.tsx
+++ ui/src/features/actorConfiguration/components/actorSettingsSection/ActorSettingsSection.tsx
@@ -1,5 +1,7 @@
-import {Grid, Typography} from "@mui/material";
 import React from "react";
+import {Button, Grid, Typography} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+
 
 const ActorSettingsSection = () => {
     return (
@@ -7,11 +9,12 @@ const ActorSettingsSection = () => {
             <Grid container direction='row' alignItems='center'>
                 <Grid item xs={5} textAlign='center'>
                     <Typography variant='h6' sx={{paddingLeft: '5px', paddingRight: '5px'}}>
-                        Settings
+                        Actions
                     </Typography>
                 </Grid>
             </Grid>
-            <Grid container item xs={12}>
+            <Grid container item xs={12} justifyContent="center">
+                <Button fullWidth variant="outlined" color="secondary" startIcon={<AddIcon />}>New</Button>
             </Grid>
             <Grid container item xs={12}>
             </Grid>
