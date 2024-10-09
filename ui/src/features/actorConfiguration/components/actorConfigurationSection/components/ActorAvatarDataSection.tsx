ebc9a8ba76a1d22e88f11ea4f52d19c8f6f8a983
Greg Warner <cats-carrying-canes@canary.net>
2024-10-08 23:16:24 -0500

    modify avatar section to use selector

--- ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorAvatarDataSection.tsx
+++ ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorAvatarDataSection.tsx
@@ -1,19 +1,16 @@
-import React, {ChangeEvent, useEffect, useState} from "react";
+import React, {ChangeEvent} from "react";
 import {Avatar, Box, IconButton, Tooltip, Typography} from "@mui/material";
 import {chatServerUrl} from "../../../../../../config";
-import {Actor} from "../../../../../types";
-import {useUpdateActorMutation, useUpdateAvatarMutation} from "../../../../../services/server/serverApi";
+import {EditableActor} from "../../../../../types";
+import {useAppDispatch, useAppSelector} from "../../../../../store/hooks";
+import {selectIsEditing, selectSelectedActor} from "../../../store/actorConfigurationSlice";
 
-const ActorAvatarDataSection = ({ actor }: { actor: Actor }) => {
-    const [updatedActor, setUpdatedActor] = useState<Actor>(actor);
-
-    useEffect(() => {
-        setUpdatedActor(actor);
-    }, [actor]);
+const ActorAvatarDataSection = () => {
+    const dispatch = useAppDispatch();
+    const actor: EditableActor = useAppSelector(selectSelectedActor);
+    const isEditing = useAppSelector(selectIsEditing);
 
     const fileInput = React.useRef<HTMLInputElement>();
-
-    const [updateAvatar] = useUpdateAvatarMutation();
 
     const onAvatarClick = () => {
         if (fileInput?.current) {
@@ -26,33 +23,48 @@ const ActorAvatarDataSection = ({ actor }: { actor: Actor }) => {
             const formData: FormData = new FormData();
             formData.append('actorId', actor.actorId.toString());
             formData.append('avatar', event.target.files[0]);
-            updateAvatar({
-                actorId: actor.actorId,
-                formData,
-            });
+            // updateAvatar({
+            //     actorId: actor.actorId,
+            //     formData,
+            // });
         }
     };
 
+    if (isEditing) {
+        return (
+            <Box display='flex' alignItems='center'>
+                <Tooltip title="Change your avatar" placement="bottom">
+                    <IconButton onClick={onAvatarClick}>
+                        <Avatar
+                            alt={`${actor.name}'s avatar`}
+                            src={`${chatServerUrl}/images/${actor.avatar}`}
+                            sx={{ width: '100px', height: '100px' }}
+                        />
+                    </IconButton>
+                </Tooltip>
+                <Typography variant='h5' component='div'>
+                    {actor.name}
+                </Typography>
+                <input
+                    ref={fileInput}
+                    type='file'
+                    style={{ display: 'none' }}
+                    onChange={onFileChange}
+                />
+            </Box>
+        );
+    }
+
     return (
         <Box display='flex' alignItems='center'>
-            <Tooltip title="Change your avatar" placement="bottom">
-                <IconButton onClick={onAvatarClick}>
-                    <Avatar
-                        alt={`${actor.name}'s avatar`}
-                        src={`${chatServerUrl}/images/${actor.avatar}`}
-                        sx={{ width: '100px', height: '100px' }}
-                    />
-                </IconButton>
-            </Tooltip>
+                <Avatar
+                    alt={`${actor.name}'s avatar`}
+                    src={`${chatServerUrl}/images/${actor.avatar}`}
+                    sx={{ width: '100px', height: '100px' }}
+                />
             <Typography variant='h5' component='div'>
                 {actor.name}
             </Typography>
-            <input
-                ref={fileInput}
-                type='file'
-                style={{ display: 'none' }}
-                onChange={onFileChange}
-            />
         </Box>
     );
 };
