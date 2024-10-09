43135e5ea078cd9042a7525c1276ee39c51f7412
Greg Warner <cats-carrying-canes@canary.net>
2024-10-08 23:34:43 -0500

    modify model section to use selector

--- ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorModelDataSection.tsx
+++ ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorModelDataSection.tsx
@@ -1,72 +1,108 @@
 import {ActorDisplayItem, ModelDataType} from '../../../types';
 import ActorDataSection from './ActorDataSection';
 import React, {useEffect, useState} from 'react';
-import {Actor, AIModel, ModelType} from '../../../../../types';
-import {Typography} from "@mui/material";
+import {Actor, AIModel, EditableActor, ModelType} from '../../../../../types';
+import {Box, Divider, Grid, Tooltip, Typography} from "@mui/material";
 import ModelSelect from "../../ModelSelect";
 import useModelType from "../../../hooks/modelTypes/useModelType";
 import useModels from "../../../hooks/useModels";
 import {useUpdateAIModelMutation} from "../../../../../services/server/serverApi";
+import {useAppDispatch, useAppSelector} from "../../../../../store/hooks";
+import {selectIsEditing, selectSelectedActor, setSelectedActor} from "../../../store/actorConfigurationSlice";
 
-const ActorModelDataSection = ({ actor }: {actor: Actor}) => {
-
-    const [model, setModel] = useState<AIModel>(actor.aiModel);
-
-    const modelType: ModelDataType = useModelType(actor.aiModel);
+const ActorModelDataSection = () => {
+    const dispatch = useAppDispatch();
+    const actor: EditableActor = useAppSelector(selectSelectedActor);
+    const isEditing = useAppSelector(selectIsEditing);
 
     const models = useModels();
 
+    const model = actor.aiModel;
+    const modelType: ModelDataType = useModelType(model);
     const modelMode = model.isLocal ? 'offline' : 'online';
 
-    const [updateModel] = useUpdateAIModelMutation();
+    // const onSave = () => {
+    //     const aiModel = modelType.appendModelType(model);
+    //     updateModel({
+    //         actorId: actor.actorId,
+    //         aiModel,
+    //     });
+    // };
 
-    useEffect(() => {
-        setModel(actor.aiModel);
-    }, [actor]);
+    // const items: ActorDisplayItem[] = [
+    //     {
+    //         label: 'Name',
+    //         value: model.modelName,
+    //         editComponent: (
 
-    const onSave = () => {
-        const aiModel = modelType.appendModelType(model);
-        updateModel({
-            actorId: actor.actorId,
-            aiModel,
-        });
-    };
-
-    const items: ActorDisplayItem[] = [
-        {
-            label: 'Name',
-            value: model.modelName,
-            editComponent: (
-                <ModelSelect
-                    selectedModel={model.modelName}
-                    handleModelChange={(event) => {
-                        const selectedModel = models.find(model => model.name === event.target.value);
-                        setModel(
-                            {...model,
-                                modelName: selectedModel.name,
-                                isLocal: selectedModel.isLocal,
-                                modelIdentifier: selectedModel.modelIdentifier,
-                            }
-                        );
-                    }}
-                />
-            )
-        },
-        {
-            label: 'Mode',
-            value: modelMode,
-            editComponent: <Typography>{modelMode}</Typography>
-        },
-        {
-            label: 'Type',
-            value: ModelType[model.modelTypeId],
-            editComponent: <Typography>{ModelType[model.modelTypeId]}</Typography>
-        },
-        ...modelType.items
-    ];
+    //         )
+    //     },
+    //     {
+    //         label: 'Mode',
+    //         value: modelMode,
+    //         editComponent: <Typography>{modelMode}</Typography>
+    //     },
+    //     {
+    //         label: 'Type',
+    //         value: ModelType[model.modelTypeId],
+    //         editComponent: <Typography>{ModelType[model.modelTypeId]}</Typography>
+    //     },
+    //     ...modelType.items
+    // ];
 
     return (
-        <ActorDataSection items={items} title='AI Model' onSave={onSave} />
+        <Box sx={{ width: '100%', pb: 2 }}>
+            <Divider textAlign='left'>AI Model</Divider>
+            <Grid pt={1} container spacing={2} justifyContent='space-between' alignItems='flex-start'>
+                    {
+                        isEditing ? (
+                            <Grid item xs={11} container spacing={2}>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>Name:</Typography>
+                                    <ModelSelect
+                                        selectedModel={actor.aiModel.modelName}
+                                        handleModelChange={(event) => {
+                                            const selectedModel = models.find(model => model.name === event.target.value);
+                                            dispatch(setSelectedActor({
+                                                ...actor,
+                                                aiModel: {
+                                                    ...model,
+                                                    modelName: selectedModel.name,
+                                                    isLocal: selectedModel.isLocal,
+                                                    modelIdentifier: selectedModel.modelIdentifier,
+                                                }
+                                            }))
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>Mode:</Typography>
+                                    <Typography variant='body1'>{modelMode}</Typography>
+                                </Grid>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>Type:</Typography>
+                                    <Typography variant='body1'>{ModelType[model.modelTypeId]}</Typography>
+                                </Grid>
+                            </Grid>
+                        ) : (
+                            <Grid item xs={11} container spacing={2}>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>Name:</Typography>
+                                    <Typography variant='body1'>{model.modelName}</Typography>
+                                </Grid>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>Mode:</Typography>
+                                    <Typography variant='body1'>{modelMode}</Typography>
+                                </Grid>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>Type:</Typography>
+                                    <Typography variant='body1'>{ModelType[model.modelTypeId]}</Typography>
+                                </Grid>
+                            </Grid>
+                        )
+                    }
+            </Grid>
+        </Box>
     );
 };
 
