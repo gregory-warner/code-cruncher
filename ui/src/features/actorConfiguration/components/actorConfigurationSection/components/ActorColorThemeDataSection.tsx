4ca38c920fce2f7d43071ff37dffb15e476e970c
Greg Warner <cats-carrying-canes@canary.net>
2024-10-08 23:45:02 -0500

    modify color theme section to use selector

--- ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorColorThemeDataSection.tsx
+++ ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorColorThemeDataSection.tsx
@@ -1,88 +1,95 @@
-import {ActorDisplayItem} from '../../../types';
-import ActorDataSection from './ActorDataSection';
-import React, {useEffect, useState} from 'react';
-import {Actor} from '../../../../../types';
-import {TextField} from "@mui/material";
-import {useUpdateActorMutation} from "../../../../../services/server/serverApi";
+import React from 'react';
+import {EditableActor, MessageCard} from '../../../../../types';
+import {Box, Divider, Grid, TextField, Typography} from "@mui/material";
+import {useAppDispatch, useAppSelector} from "../../../../../store/hooks";
+import {selectIsEditing, selectSelectedActor, setSelectedActor} from "../../../store/actorConfigurationSlice";
 
-const ActorColorThemeDataSection = ({ actor }: {actor: Actor}) => {
-    const [updatedActor, setUpdatedActor] = useState<Actor>(actor);
+const ActorColorThemeDataSection = () => {
+    const dispatch = useAppDispatch();
+    const actor: EditableActor = useAppSelector(selectSelectedActor);
+    const isEditing = useAppSelector(selectIsEditing);
 
-    useEffect(() => {
-        setUpdatedActor(actor);
-    }, [actor]);
+    const messageCardStyle: MessageCard|null = actor?.colorTheme.messageCard ?? null;
 
-    const [updateActor] = useUpdateActorMutation();
-
-    const onSave = () => {
-        updateActor({
-            actorId: actor.actorId,
-            ...updatedActor,
-        });
-    };
-
-    const items: ActorDisplayItem[] = [
-        {
-            label: 'Name',
-            value: updatedActor.colorTheme.messageCard.nameColor,
-            editComponent: (
-                <TextField
-                    defaultValue={updatedActor.colorTheme.messageCard.nameColor}
-                    onChange={(event) => setUpdatedActor({
-                        ...updatedActor,
-                        colorTheme: {
-                            ...updatedActor.colorTheme,
-                            messageCard: {
-                                ...updatedActor.colorTheme.messageCard,
-                                nameColor: event.target.value
-                            }
-                        }
-                    })}
-                />
-            )
-        },
-        {
-            label: 'Border',
-            value: updatedActor.colorTheme.messageCard.borderColor,
-            editComponent: (
-                <TextField
-                    defaultValue={updatedActor.colorTheme.messageCard.borderColor}
-                    onChange={(event) => setUpdatedActor({
-                        ...updatedActor,
-                        colorTheme: {
-                            ...updatedActor.colorTheme,
-                            messageCard: {
-                                ...updatedActor.colorTheme.messageCard,
-                                borderColor: event.target.value
-                            }
-                        }
-                    })}
-                />
-            )
-        },
-        {
-            label: 'Contents',
-            value: updatedActor.colorTheme.messageCard.contentsColor,
-            editComponent: (
-                <TextField
-                    defaultValue={updatedActor.colorTheme.messageCard.contentsColor}
-                    onChange={(event) => setUpdatedActor({
-                        ...updatedActor,
-                        colorTheme: {
-                            ...updatedActor.colorTheme,
-                            messageCard: {
-                                ...updatedActor.colorTheme.messageCard,
-                                contentsColor: event.target.value
-                            }
-                        }
-                    })}
-                />
-            )
-        },
-    ];
+    if (!messageCardStyle) {
+        return <Box />;
+    }
 
     return (
-        <ActorDataSection items={items} title='Color Theme' onSave={onSave} />
+        <Box sx={{ width: '100%', pb: 2 }}>
+            <Divider textAlign='left'>Color Theme</Divider>
+            <Grid pt={1} container spacing={2} justifyContent='space-between' alignItems='flex-start'>
+                {
+                    isEditing ? (
+                        <Grid item xs={11} container spacing={2}>
+                            <Grid item xs={4} display='flex' alignItems='center'>
+                                <Typography variant='body2' mr={1}>Name:</Typography>
+                                <TextField
+                                    value={messageCardStyle.nameColor}
+                                    onChange={(event) => dispatch(setSelectedActor({
+                                        ...actor,
+                                        colorTheme: {
+                                            ...actor.colorTheme,
+                                            messageCard: {
+                                                ...actor.colorTheme.messageCard,
+                                                nameColor: event.target.value
+                                            }
+                                        }
+                                    }))}
+                                />
+                            </Grid>
+                            <Grid item xs={4} display='flex' alignItems='center'>
+                                <Typography variant='body2' mr={1}>Border:</Typography>
+                                <TextField
+                                    value={messageCardStyle.borderColor}
+                                    onChange={(event) => dispatch(setSelectedActor({
+                                        ...actor,
+                                        colorTheme: {
+                                            ...actor.colorTheme,
+                                            messageCard: {
+                                                ...actor.colorTheme.messageCard,
+                                                borderColor: event.target.value
+                                            }
+                                        }
+                                    }))}
+                                />
+                            </Grid>
+                            <Grid item xs={4} display='flex' alignItems='center'>
+                                <Typography variant='body2' mr={1}>Contents:</Typography>
+                                <TextField
+                                    value={messageCardStyle.contentsColor}
+                                    onChange={(event) => dispatch(setSelectedActor({
+                                        ...actor,
+                                        colorTheme: {
+                                            ...actor.colorTheme,
+                                            messageCard: {
+                                                ...actor.colorTheme.messageCard,
+                                                contentsColor: event.target.value
+                                            }
+                                        }
+                                    }))}
+                                />
+                            </Grid>
+                        </Grid>
+                    ) : (
+                        <Grid item xs={11} container spacing={2}>
+                            <Grid item xs={4} display='flex' alignItems='center'>
+                                <Typography variant='body2' mr={1}>Name:</Typography>
+                                <Typography variant='body1'>{messageCardStyle.nameColor}</Typography>
+                            </Grid>
+                            <Grid item xs={4} display='flex' alignItems='center'>
+                                <Typography variant='body2' mr={1}>Border:</Typography>
+                                <Typography variant='body1'>{messageCardStyle.borderColor}</Typography>
+                            </Grid>
+                            <Grid item xs={4} display='flex' alignItems='center'>
+                                <Typography variant='body2' mr={1}>Contents:</Typography>
+                                <Typography variant='body1'>{messageCardStyle.contentsColor}</Typography>
+                            </Grid>
+                        </Grid>
+                    )
+                }
+            </Grid>
+        </Box>
     );
 };
 
