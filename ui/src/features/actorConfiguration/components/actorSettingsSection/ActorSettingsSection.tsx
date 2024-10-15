9e6947a781b9fcf9e2002cc873ec2ae410c361b4
Greg Warner <cats-carrying-canes@canary.net>
2024-10-14 23:02:06 -0500

    add onSave and onEdit functionality

--- ui/src/features/actorConfiguration/components/actorSettingsSection/ActorSettingsSection.tsx
+++ ui/src/features/actorConfiguration/components/actorSettingsSection/ActorSettingsSection.tsx
@@ -5,18 +5,25 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import SaveIcon from '@mui/icons-material/Save';
 import DeleteIcon from '@mui/icons-material/Delete';
 import {useUpdateActorMutation} from "../../../../services/server/serverApi";
+import {useAppDispatch, useAppSelector} from "../../../../store/hooks";
+import {selectIsEditing, selectSelectedActor, setIsEditing} from "../../store/actorConfigurationSlice";
 
 const ActorSettingsSection = () => {
     const theme = useTheme();
+    const dispatch = useAppDispatch();
+
+    const isEditing = useAppSelector(selectIsEditing);
+    const actor = useAppSelector(selectSelectedActor);
 
     const [updateActor] = useUpdateActorMutation();
 
-    // const onSave = () => {
-    //     updateActor({
-    //         actorId: actor.actorId,
-    //         ...updatedActor,
-    //     });
-    // };
+    const onSave = () => {
+        updateActor(actor);
+    };
+
+    const onEdit = () => {
+        dispatch(setIsEditing(true));
+    };
 
     return (
         <Grid container>
@@ -34,10 +41,32 @@ const ActorSettingsSection = () => {
                 <Button fullWidth variant="outlined" color='inherit' startIcon={<ContentCopyIcon />}>Clone</Button>
             </Grid>
             <Grid container item xs={12} justifyContent="center" p={1} sx={{ color: theme.palette.secondary.main }}>
-                <Button fullWidth variant="outlined" color="secondary" startIcon={<ContentCopyIcon />}>Edit</Button>
+                {
+                    isEditing ? (
+                        <Button
+                            fullWidth
+                            variant="outlined"
+                            color="secondary"
+                            onClick={onEdit}
+                            startIcon={<ContentCopyIcon />}
+                        >
+                            Cancel
+                        </Button>
+                    ) : (
+                        <Button
+                            fullWidth
+                            variant="outlined"
+                            color="secondary"
+                            onClick={onEdit}
+                            startIcon={<ContentCopyIcon />}
+                        >
+                            Edit
+                        </Button>
+                    )
+                }
             </Grid>
             <Grid container item xs={12} justifyContent="center" p={1} sx={{ color: theme.palette.secondary.main }}>
-                <Button fullWidth variant="outlined" color="secondary" startIcon={<SaveIcon />}>Save</Button>
+                <Button fullWidth variant="outlined" color="secondary" onClick={onSave} startIcon={<SaveIcon />}>Save</Button>
             </Grid>
             <Grid container item xs={12} justifyContent="center" p={1} >
                 <Button fullWidth variant="outlined" color="error" startIcon={<DeleteIcon />}>Delete</Button>
