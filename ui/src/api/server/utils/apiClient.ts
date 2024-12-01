10f30c71e3cfc23880c095c537e73160105ff03c
Greg Warner <cats-carrying-canes@canary.net>
2024-11-30 23:58:58 -0600

    remove legacy api service

--- ui/src/api/server/utils/apiClient.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosInstance } from 'axios';
-
-const client: AxiosInstance = axios.create({
-    baseURL: `http://localhost:${import.meta.env.VITE_SERVER_PORT}`,
-});
-
-export const apiClient = client;
