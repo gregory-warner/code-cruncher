50b623b2738dc5b7c2d53ed5e7be6174f194903d
Greg Warner <cats-carrying-canes@canary.net>
2024-12-01 23:33:53 -0600

    add username to config edit

--- ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorGeneralDataSection.tsx
+++ ui/src/features/actorConfiguration/components/actorConfigurationSection/components/ActorGeneralDataSection.tsx
@@ -17,39 +17,52 @@ const ActorGeneralDataSection = () => {
                 <Grid item xs={11} container spacing={2}>
                     {
                         isEditing ? (
-                            <Grid item xs={4} display='flex' alignItems='center'>
-                                <Typography variant='body2' mr={1}>{`Name:`}</Typography>
-                                <TextField
-                                    value={actor.name}
-                                    onChange={(event) => dispatch(setSelectedActor({
-                                        ...actor,
-                                        name: event.target.value
-                                    }))}
-                                />
+                            <Grid container>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>{`Name:`}</Typography>
+                                    <TextField
+                                        value={actor.name}
+                                        onChange={(event) => dispatch(setSelectedActor({
+                                            ...actor,
+                                            name: event.target.value
+                                        }))}
+                                    />
+                                </Grid>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>{`Username:`}</Typography>
+                                    <TextField
+                                        value={actor.username}
+                                        onChange={(event) => dispatch(setSelectedActor({
+                                            ...actor,
+                                            username: event.target.value
+                                        }))}
+                                    />
+                                </Grid>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>{`Title:`}</Typography>
+                                    <TextField
+                                        value={actor.title}
+                                        onChange={(event) => dispatch(setSelectedActor({
+                                            ...actor,
+                                            title: event.target.value
+                                        }))}
+                                    />
+                                </Grid>
                             </Grid>
                         ) : (
-                            <Grid item xs={4} display='flex' alignItems='center'>
-                                <Typography variant='body2' mr={1}>{`Name:`}</Typography>
-                                {actor.name}
-                            </Grid>
-                        )
-                    }
-                    {
-                        isEditing ? (
-                            <Grid item xs={4} display='flex' alignItems='center'>
-                                <Typography variant='body2' mr={1}>{`Title:`}</Typography>
-                                <TextField
-                                    value={actor.title}
-                                    onChange={(event) => dispatch(setSelectedActor({
-                                        ...actor,
-                                        title: event.target.value
-                                    }))}
-                                />
-                            </Grid>
-                        ) : (
-                            <Grid item xs={4} display='flex' alignItems='center'>
-                                <Typography variant='body2' mr={1}>{`Title:`}</Typography>
-                                {actor.title}
+                            <Grid container>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>{`Name:`}</Typography>
+                                    {actor.name}
+                                </Grid>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>{`Username:`}</Typography>
+                                    {actor.username}
+                                </Grid>
+                                <Grid item xs={4} display='flex' alignItems='center'>
+                                    <Typography variant='body2' mr={1}>{`Title:`}</Typography>
+                                    {actor.title}
+                                </Grid>
                             </Grid>
                         )
                     }
