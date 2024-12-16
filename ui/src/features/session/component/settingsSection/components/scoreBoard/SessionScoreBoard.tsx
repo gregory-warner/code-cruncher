f2c4a9d1bdebc61ca90a8dba9c90d241755e7208
Greg Warner <cats-carrying-canes@canary.net>
2024-12-15 23:58:05 -0600

    init score board

---
+++ ui/src/features/session/component/settingsSection/components/scoreBoard/SessionScoreBoard.tsx
@@ -0,0 +1,14 @@
+import {Box, Grid, Typography} from "@mui/material";
+
+const SessionScoreBoard = () => {
+
+    return (
+        <Grid container direction='row'>
+            <Grid item xs={12}>
+                <Typography variant='h4'>Score</Typography>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default SessionScoreBoard;
