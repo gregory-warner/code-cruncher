705360f0d3badfa6a5bfde276108d26ccc7b11e2
Greg Warner <cats-carrying-canes@canary.net>
2024-12-16 23:24:20 -0600

    use digital font for score board values

--- ui/src/features/session/component/settingsSection/components/scoreBoard/SessionScore.tsx
+++ ui/src/features/session/component/settingsSection/components/scoreBoard/SessionScore.tsx
@@ -6,7 +6,7 @@ const SessionScore = ({title, score}: {title: string, score: number}) => {
     return (
         <Grid container direction='column' alignItems='center'>
             <Typography variant='h6'>{title}</Typography>
-            <Typography variant='subtitle2' color='grey'>{score}</Typography>
+            <Typography variant='subtitle2' color='grey' sx={{ fontFamily: 'Digital-7', fontSize: '2rem' }}>{score}</Typography>
         </Grid>
     );
 };
