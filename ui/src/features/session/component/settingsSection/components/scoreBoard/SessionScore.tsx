d4e091e7e9ece53c8918537311c6070ecb3e81e6
Greg Warner <cats-carrying-canes@canary.net>
2024-12-17 22:38:54 -0600

    add color prop to session score

--- ui/src/features/session/component/settingsSection/components/scoreBoard/SessionScore.tsx
+++ ui/src/features/session/component/settingsSection/components/scoreBoard/SessionScore.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {Grid, Typography} from "@mui/material";
 
-const SessionScore = ({title, score}: {title: string, score: number}) => {
+const SessionScore = ({title, score, color='grey'}: { title: string, score: number, color: string }) => {
 
     return (
         <Grid container direction='column' alignItems='center'>
             <Typography variant='h6'>{title}</Typography>
-            <Typography variant='subtitle2' color='grey' sx={{ fontFamily: 'Digital-7', fontSize: '2rem' }}>{score}</Typography>
+            <Typography  variant='subtitle2' color={`${color}`} sx={{ fontFamily: 'Digital-7', fontSize: '2rem' }}>{score}</Typography>
         </Grid>
     );
 };
