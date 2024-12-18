19023f7eed0572501c382114db47c43ace9f9ba0
Greg Warner <cats-carrying-canes@canary.net>
2024-12-17 22:38:35 -0600

    set correct/incorrect score components with font color

--- ui/src/features/session/component/settingsSection/components/scoreBoard/SessionScoreBoard.tsx
+++ ui/src/features/session/component/settingsSection/components/scoreBoard/SessionScoreBoard.tsx
@@ -1,21 +1,24 @@
 import React from "react";
-import {Box, Grid, Paper, Typography} from "@mui/material";
+import {Box, Grid, Paper, useTheme} from "@mui/material";
 import SessionScore from "./SessionScore";
 
 const SessionScoreBoard = () => {
+    const theme = useTheme();
 
     return (
-        <Paper elevation={3} sx={{ border: '5px solid black', p: 2, width: '100%' }}>
-            <Grid container direction='row' justifyContent='space-around'>
-                <Grid item xs={5}>
-                    <SessionScore title={'test'} score={1} />
+        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <Paper elevation={3} sx={{ border: `5px solid ${theme.palette.background.default}`, pl: 1, pr: 1, width: '80%' }}>
+                <Grid container direction='row' justifyContent='space-around'>
+                    <Grid item xs={5}>
+                        <SessionScore title={'Correct'} score={1} color={theme.palette.primary.main} />
+                    </Grid>
+                    <Grid item xs={5}>
+                        <SessionScore title={'Incorrect'} score={23} color={theme.palette.error.main} />
+                    </Grid>
                 </Grid>
-                <Grid item xs={5}>
-                    <SessionScore title={'pass'} score={23} />
-                </Grid>
-            </Grid>
-        </Paper>
-    );
+            </Paper>
+        </Box>
+        );
 };
 
 export default SessionScoreBoard;
