c3f1b6c609e6d14287cb3b1b0071367310c2564b
Greg Warner <cats-carrying-canes@canary.net>
2024-12-26 23:34:33 -0600

    add message event details model

---
+++ server/models/messageEventDetails.js
@@ -0,0 +1,40 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../db.js';
+
+const MessageEventDetails = sequelize.define('message_event_details', {
+    messageEventId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        field: 'message_event_id',
+    },
+    questionId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: 'question_id',
+        comment: 'used for the question and the associated answer(s)'
+    },
+    resultId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        field: 'result_id',
+        commend: '0 - incorrect, 1 - correct, 2 - partially correct'
+    },
+    duration: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        field: 'duration',
+        comment: 'time taken, in seconds, to submit the answer'
+    },
+},{
+    paranoid: true,
+    timestamps: true,
+    indexes: [
+        {
+            fields: ['question_id', 'result_id'],
+            name: 'idx_quest_result',
+        },
+    ],
+});
+
+export default MessageEventDetails;
