08c3bc2860ecb6635a817320868977cfcde1fcb8
Greg Warner <cats-carrying-canes@canary.net>
2024-12-27 00:01:54 -0600

    add service to add message event details

--- server/services/messageEventDetailsService.js
+++ server/services/messageEventDetailsService.js
@@ -1,3 +1,5 @@
+import {MessageEventDetails} from "../models/models.js";
+
 export const addDetails = async (details) => {
     const questionId = parseInt(details.questionId ?? '');
     const messageId = parseInt(details.messageId ?? '');
@@ -5,4 +7,8 @@ export const addDetails = async (details) => {
     if (!questionId || !messageId) {
         throw new Error('Missing required parameters');
     }
+
+    MessageEventDetails.create({
+        questionId, messageId
+    })
 };
