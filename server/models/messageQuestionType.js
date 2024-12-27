a274e095c29a68fd458f6f442705f4d954d63ee3
Greg Warner <cats-carrying-canes@canary.net>
2024-12-26 23:34:49 -0600

    add message question type model

---
+++ server/models/messageQuestionType.js
@@ -0,0 +1,22 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../db.js';
+
+const MessageQuestionType = sequelize.define('message_question_type', {
+    messageEventId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        field: 'message_event_id',
+    },
+    questionType: {
+        type: DataTypes.TEXT,
+        primaryKey: true,
+        allowNull: false,
+        field: 'question_type',
+    },
+},{
+    paranoid: true,
+    timestamps: true,
+});
+
+export default MessageQuestionType;
