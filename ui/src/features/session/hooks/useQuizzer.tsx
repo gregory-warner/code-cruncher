201866a0c6d40a25ebd80087fb8a0c37ef766969
Greg Warner <cats-carrying-canes@canary.net>
2025-01-05 23:23:32 -0600

    add logic to handle quizzer response

--- ui/src/features/session/hooks/useQuizzer.tsx
+++ ui/src/features/session/hooks/useQuizzer.tsx
@@ -5,27 +5,34 @@ import {
     useAddMessageQuestionTypesMutation
 } from "../../../services/server/serverApi";
 
+interface QuestionResult {
+    questionId: number;
+    resultId: number;
+}
 
 export const useQuizzer = () => {
     const questionMessage = 'CODE CRUNCHER - QUESTION';
-    const quizzerResponses = ['INCORRECT!', 'PARTIALLY CORRECT!', 'CORRECT!'];
+
+    const quizzerResponseScores = {
+        'INCORRECT!': -1,
+        'CORRECT!': 1,
+    };
 
     const [addEvent] = useAddMessageEventDetailsMutation();
     const [addQuestionTypes] = useAddMessageQuestionTypesMutation();
 
     const isQuizQuestion = (message: Message): boolean => {
         const regexQuestion = new RegExp(`^${questionMessage} \d+`);
-        return /^CODE CRUNCHER - QUESTION \d+/.test(message.content);
-        // return message.content.includes(questionMessage);
+        return /CODE CRUNCHER - QUESTION \d+/.test(message.content);
     };
 
     const isQuizzerResponse = (message: Message): boolean => {
-        const regexResponse = new RegExp(`^QUESTION (\\d+):\\s(${quizzerResponses.join('|')})\\b`);
+        const regexResponse = new RegExp(`^QUESTION (\\d+):\\s(${Object.keys(quizzerResponseScores).join('|')})`);
         return regexResponse.test(message.content);
     };
 
     const handleQuizQuestion = async (message: Message): Promise<Message> => {
-        const eventDetails = await addEvent(getEventDetails(message)).unwrap();
+        const eventDetails = await addEvent(getQuizQuestionEventDetails(message)).unwrap();
 
         const questionTypes = getQuestionTypes(message);
 
@@ -34,8 +41,22 @@ export const useQuizzer = () => {
         return message;
     };
 
-    const getEventDetails = (message: Message): MessageEventDetailsRequest => {
-        const questionId =  parseInt(/QUESTION (\d+)/.exec(message.content)?.[1] ?? '0');
+    const handleQuizzerResponse = async (message: Message): Promise<Message> => {
+        const questionResult = getQuestionResult(message);
+        const eventDetails = await addEvent({ ...questionResult, messageId: message.messageId }).unwrap();
+        return message;
+    };
+
+    const getQuestionResult = (message: Message): QuestionResult => {
+        const match = message.content.match(/QUESTION (\d+): (.*?)(\n|$)/);
+        const questionId = Number(match[1]);
+        const response = match[2].trim();
+        const resultId = quizzerResponseScores[response as keyof typeof quizzerResponseScores] ?? 0;
+        return { questionId, resultId };
+    };
+
+    const getQuizQuestionEventDetails = (message: Message): MessageEventDetailsRequest => {
+        const questionId =  parseInt(/CODE CRUNCHER - QUESTION (\d+)/.exec(message.content)?.[1] ?? '0');
 
         return {
             messageId: message.messageId,
@@ -44,12 +65,15 @@ export const useQuizzer = () => {
     };
 
     const getQuestionTypes = (message: Message): string[] => {
-        return (/(Data Types|Datatype): ([\w,\s]+)(\n){2}/.exec(message.content)?.[1] ?? '').split(', ');
+        const dataTypesMatch = message.content.match(/Data Types:(.*?)(\n|$)/);
+        const dataTypes = dataTypesMatch[1].trim();
+        return dataTypes.split(', ');
     };
 
     return {
         isQuizQuestion,
         handleQuizQuestion,
         isQuizzerResponse,
+        handleQuizzerResponse,
     };
 };
