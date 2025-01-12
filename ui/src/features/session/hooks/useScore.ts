542ac16017d88068173b0823a374ea988d1fd1b2
Greg Warner <cats-carrying-canes@canary.net>
2025-01-11 21:38:48 -0600

    add logic to calculate score from message event details

--- ui/src/features/session/hooks/useScore.ts
+++ ui/src/features/session/hooks/useScore.ts
@@ -1,17 +1,39 @@
-import {useLazyGetMessagesQuery} from "../../../services/server/serverApi";
-
-interface Scores {
-    correct: number;
-    incorrect: number;
-}
+import {useLazyGetSessionMessageEventDetailsQuery} from "../../../services/server/serverApi";
+import { Scores } from "../types";
 
 const useScore = () => {
-    const [messages] = useLazyGetMessagesQuery();
+    const [sessionDetails] = useLazyGetSessionMessageEventDetailsQuery();
 
+    const getScores = async (sessionId: number): Promise<Scores> => {
+        if (!sessionId) {
+            return { correct: 0, incorrect: 0 };
+        }
 
-    // const getScores: Scores = () => {
-    //
-    // };
+        const messageDetails = await sessionDetails(sessionId).unwrap();
+        
+        let correct = 0;
+        let incorrect = 0;
+
+        messageDetails.forEach(details => {
+            details.forEach(eventDetails => {
+                if (Number.isInteger(eventDetails.resultId)) {
+                    if (eventDetails.resultId > 0) {
+                        correct+=1;
+                    }
+                    if (eventDetails.resultId < 0) {
+                        incorrect+=1;
+                    }
+                }
+            })
+        });
+
+        return { correct, incorrect };
+    };
+
+    return {
+        getScores,
+    };
+
 };
 
 export default useScore;
