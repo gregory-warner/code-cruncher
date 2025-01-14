ab876bcabd0670c4f977c499eb309cde77a66ace
Greg Warner <cats-carrying-canes@canary.net>
2025-01-13 23:30:45 -0600

    remove unused functions

--- server/services/aiModelService.js
+++ server/services/aiModelService.js
@@ -1,6 +1,6 @@
 import {Actor, AIModel, ImageModel, LanguageModel, TextModel} from "../models/models.js";
 import validator from "validator";
-import {createLanguageModel} from "./languageModelService.js";
+import {createLanguageModel, updateModelType} from "./languageModelService.js";
 
 export const modelTypes = {
     language: 0,
@@ -21,101 +21,6 @@ export const getModelDetails = async (aiModel) => {
     }
 };
 
-/**
- * Adds a model to the database. parameter needs to have a typeDetails property with the expected values
- * for the model type e.g. ('size', 'num', 'quality', 'style') for ImageModel type.
- *
- * @param model
- * @returns {Promise<{success: boolean, model: any}>}
- */
-export const addModel = async (model, transaction) => {
-    if (!'typeDetails' in model) {
-        throw new Error(`missing typeDetails for model with name ${model.name}`);
-    }
-
-    const modelProviderDetails = getModelProviderDetails(model);
-
-    const aiModel = await AIModel.create(modelProviderDetails, { transaction });
-
-    if (!aiModel instanceof AIModel) {
-        throw new Error(`unable to create AI Model with type ${modelProviderDetails.modelTypeId} and name ${model.name}`);
-    }
-
-    const typeDetails = {
-        ...model.typeDetails,
-        modelId: aiModel.modelId,
-    };
-
-    const typeModel = await createTypeModel(modelProviderDetails.modelTypeId, typeDetails, transaction);
-
-    return {
-        ...aiModel.toJSON(),
-        modelType: {
-            ...typeModel.toJSON(),
-        }
-    };
-};
-
-const createTypeModel = async (typeId, typeDetails, transaction) => {
-    switch (typeId) {
-        case modelTypes.language:
-            return await LanguageModel.create({
-                ...typeDetails,
-            }, { transaction });
-        case modelTypes.image:
-            return await ImageModel.create({
-                ...typeDetails,
-            });
-        case modelTypes.text:
-            return await TextModel.create({
-                ...typeDetails,
-            });
-        default:
-            throw new Error(`unable to create type model with model id ${typeDetails.modelId}`);
-    }
-};
-
-const getModelProviderDetails = (model) => {
-    const typeId = modelTypes[getModelType(model)];
-
-    const ollamaModel = 'details' in model;
-    const openaiModel = 'object' in model && 'owned_by' in model;
-
-    if (ollamaModel) {
-        return {
-            modelName: model.name,
-            modelIdentifier: 'ollama',
-            modelTypeId: typeId,
-            isLocal: true,
-        };
-    }
-
-    if (openaiModel) {
-        return {
-            modelName: model.id,
-            modelIdentifier: 'openai',
-            modelTypeId: typeId,
-            isLocal: false,
-        };
-    }
-
-    throw new Error(`Unable to determine model provider for model id ${model.id}`);
-};
-
-export const getModelType = (model) => {
-    const typeDetails = model.typeDetails;
-
-    if ('size' in typeDetails && 'quality' in typeDetails) {
-        return 'image';
-    }
-
-    if ('maxTokens' in typeDetails && 'temperature' in typeDetails) {
-        return 'language';
-    }
-
-    throw new Error('Missing required fields for type details');
-};
-
 export const getValidatedModel = (model, requiredFields = ['modelName']) => {
     if (!model || typeof model !== 'object') {
         throw new Error('Invalid model data: ' + JSON.stringify(model));
@@ -132,34 +37,6 @@ export const getValidatedModel = (model, requiredFields = ['modelName']) => {
     }
 
     return validatedPrompt;
-};
-
-
-export const createModelType = async (modelId, model) => {
-    if (model.languageModel) {
-        const languageModel = getValidatedModel(model.languageModel, ['maxTokens', 'temperature', 'frequencyPenalty']);
-        return await LanguageModel.create({
-            modelId,
-            ...languageModel,
-        });
-    }
-
-    throw new Error(`Unable to create model type with model id ${modelId}`);
-};
-
-const updateLanguageModel = async (model) => {
-    const languageModel = await LanguageModel.findOne({ where: { modelId: model.modelId } });
-    const lang = model.languageModel;
-    languageModel.maxTokens = lang.maxTokens;
-    languageModel.temperature = lang.temperature;
-    languageModel.frequencyPenalty = lang.frequencyPenalty;
-    await languageModel.save();
-};
-
-const updateModelType = async (model) => {
-    if (model.languageModel) {
-        await updateLanguageModel(model);
-    }
 };
 
 export const updateActorModel = async (actorId, model) => {
