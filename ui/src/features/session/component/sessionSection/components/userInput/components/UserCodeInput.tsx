dcaade3c3ba9670851891ed9647c660404f9ef4d
Greg Warner <cats-carrying-canes@canary.net>
2025-01-13 21:27:10 -0600

    use session type for code block language context

--- ui/src/features/session/component/sessionSection/components/userInput/components/UserCodeInput.tsx
+++ ui/src/features/session/component/sessionSection/components/userInput/components/UserCodeInput.tsx
@@ -5,16 +5,24 @@ import React from "react";
 import {Box} from "@mui/material";
 import useMessageInput from "../hooks/useMessageInput";
 import UserInputFooter from "./UserInputFooter/UserInputFooter";
+import {useLazyGetSessionQuery} from "../../../../../../../services/server/serverApi";
+import {selectSessionId} from "../../../../../sessionSlice";
+import {SessionType} from "../../../../../../../types";
 
 const UserCodeInput = ({ language } : { language: string }) => {
+    const sessionId = useAppSelector(selectSessionId);
 
     const themeMode = useAppSelector(selectMode);
 
     const { input, onInputChange, addMessageToSession } = useMessageInput();
 
+    const [getSession] = useLazyGetSessionQuery();
 
     const onSend = async () => {
-        const content = `\`\`\`\n${input}\n\`\`\``;
+        const session = await getSession(sessionId).unwrap();
+        const sessionType = SessionType[session.sessionTypeId]?.toLowerCase() ?? '';
+
+        const content = `\`\`\`${sessionType}\n${input}\n\`\`\``;
         await addMessageToSession(content);
     }
 
