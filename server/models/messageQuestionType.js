26abc2734fc07f220eaa1e28e1e818da6c51f487
Greg Warner <cats-carrying-canes@canary.net>
2025-01-13 21:20:29 -0600

    replace composite primary key with unique

--- server/models/messageQuestionType.js
+++ server/models/messageQuestionType.js
@@ -2,16 +2,20 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../db.js';
 
 const MessageQuestionType = sequelize.define('message_question_type', {
-    messageEventId: {
+    messageQuestionTypeId: {
         type: DataTypes.INTEGER,
         primaryKey: true,
+        autoIncrement: true,
+        field: 'message_question_type_id',
+    },
+    messageEventId: {
+        type: DataTypes.INTEGER,
         allowNull: false,
         field: 'message_event_id',
         comment: 'references message_event_details.message_event_id'
     },
     questionType: {
         type: DataTypes.TEXT,
-        primaryKey: true,
         allowNull: false,
         field: 'question_type',
     },
