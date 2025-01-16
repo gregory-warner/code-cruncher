9cb114322eb70a0bb0d6113cb9fb96d74eeef9d8
Greg Warner <cats-carrying-canes@canary.net>
2025-01-15 23:32:38 -0600

    add prompts table to prompt layout

--- ui/src/features/prompt/Prompts.tsx
+++ ui/src/features/prompt/Prompts.tsx
@@ -1,12 +1,13 @@
 import AppLayout from "../../app/components/layout/AppLayout";
 import AssistantsSection from "../assistant/component/assistantsSection/AssistantsSection";
+import PromptsTable from "./components/promptsTable/PromptsTable";
 
 const Prompts = () => {
 
     return (
         <AppLayout
             leftPanel={<AssistantsSection />}
-            center={<></>}
+            center={<PromptsTable />}
             rightPanel={<></>}
         />
     );
