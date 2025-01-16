a78e2ee9d48b8449c5c77eac3d3755cd04ecf82c
Greg Warner <cats-carrying-canes@canary.net>
2025-01-15 23:32:27 -0600

    add prompt table

---
+++ ui/src/features/prompt/components/promptsTable/PromptsTable.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Box, Table, TableContainer} from "@mui/material";
+import PromptsTableHead from "./components/PromptsTableHead";
+import {EditableActor} from "../../../../types";
+import {useAppSelector} from "../../../../store/hooks";
+import {selectSelectedActor} from "../../../assistant/assistantSlice";
+
+const PromptsTable = () => {
+
+    const actor: EditableActor|null = useAppSelector(selectSelectedActor)
+
+    if (!actor) {
+        return <Box></Box>;
+    }
+
+    return (
+        <TableContainer>
+            <Table>
+                <PromptsTableHead />
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default PromptsTable;
