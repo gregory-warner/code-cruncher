7d6dd85e536a7f3695fbf95627830d307453e5c1
Greg Warner <cats-carrying-canes@canary.net>
2025-01-27 22:47:38 -0600

    set prompts for table

--- ui/src/features/prompt/components/promptsTable/components/PromptsTableBody.tsx
+++ ui/src/features/prompt/components/promptsTable/components/PromptsTableBody.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from "react";
-import usePrompts from "../../../hooks/usePrompts";
 import {EditableActor, Prompt} from "../../../../../types";
 import {useAppSelector} from "../../../../../store/hooks";
 import {selectSelectedActor} from "../../../../assistant/assistantSlice";
@@ -11,18 +10,12 @@ const PromptsTableBody = () => {
 
     const [prompts, setPrompts] = React.useState<Prompt[]>([]);
 
-    const { getPrompts } = usePrompts();
-
-    const updatePrompts = async (actorId: number) => {
-        setPrompts(await getPrompts(actorId));
-    }
-
     useEffect(() => {
         if (!actor.actorId) {
             return;
         }
 
-        updatePrompts(actor.actorId);
+        setPrompts(actor.prompts)
     }, [actor]);
 
     return (
