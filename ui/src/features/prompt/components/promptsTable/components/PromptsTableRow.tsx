8763dedc1c97b30eb2b589b9b95dbce1460b1df7
Greg Warner <cats-carrying-canes@canary.net>
2025-01-27 22:47:11 -0600

    update prompt table dates to readable

--- ui/src/features/prompt/components/promptsTable/components/PromptsTableRow.tsx
+++ ui/src/features/prompt/components/promptsTable/components/PromptsTableRow.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Prompt} from "../../../../../types";
 import {TableCell, TableRow} from "@mui/material";
+import {getFormattedDate} from "../../../util";
 
 const PromptsTableRow = ({prompt}: {prompt: Prompt}) => {
 
@@ -10,8 +11,8 @@ const PromptsTableRow = ({prompt}: {prompt: Prompt}) => {
             <TableCell component='th' scope='row'>{prompt.promptName}</TableCell>
             <TableCell component='th' scope='row'>{prompt.prompt}</TableCell>
             <TableCell component='th' scope='row'>{prompt.postfix}</TableCell>
-            <TableCell component='th' scope='row'>{prompt.createdAt}</TableCell>
-            <TableCell component='th' scope='row'>{prompt.deletedAt}</TableCell>
+            <TableCell component='th' scope='row'>{getFormattedDate(prompt.createdAt)}</TableCell>
+            <TableCell component='th' scope='row'>{getFormattedDate(prompt.deletedAt)}</TableCell>
         </TableRow>
     );
 };
