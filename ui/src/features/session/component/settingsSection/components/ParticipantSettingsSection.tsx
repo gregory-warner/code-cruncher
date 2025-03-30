d52d825c119fd38adfc7ddc947d65f1ca10e5967
Greg Warner <cats-carrying-canes@canary.net>
2025-03-30 00:05:49 -0500

    separate assistant and user settings return section

--- ui/src/features/session/component/settingsSection/components/ParticipantSettingsSection.tsx
+++ ui/src/features/session/component/settingsSection/components/ParticipantSettingsSection.tsx
@@ -3,24 +3,27 @@ import React from "react";
 import UserParticipantSettings from "./UserParticipantSettings";
 import { Actor, User } from "../../../../../types";
 import ActorParticipantSettings from "./ActorParticipantSettings";
-import {useParticipant} from "../../../hooks/useParticipant";
+import {isActor} from "../../../../../utils/util";
+import {useSessionParticipant} from "../../../hooks/useSessionParticipant";
 
 const ParticipantSettingsSection = () => {
-    const { selectedParticipant, isActor } = useParticipant();
+    const { selectedParticipant } = useSessionParticipant();
 
     if (!selectedParticipant) {
         return <Box></Box>;
     }
 
+    if (isActor(selectedParticipant)) {
+        return (
+            <Box sx={{ width: '100%', pl: 1 }}>
+                <ActorParticipantSettings actor={selectedParticipant as Actor} />
+            </Box>
+        );
+    }
+
     return (
         <Box sx={{ width: '100%', pl: 1 }}>
-            {
-                isActor ? (
-                    <ActorParticipantSettings actor={selectedParticipant as Actor} />
-                ) : (
-                    <UserParticipantSettings user={selectedParticipant as User} />
-                )
-            }
+            <UserParticipantSettings user={selectedParticipant as User} />
         </Box>
     );
 };
