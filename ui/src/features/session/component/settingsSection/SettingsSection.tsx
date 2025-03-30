3eccb6fc33e744cbe6c27ac52cef6e9e87ee1cd9
Greg Warner <cats-carrying-canes@canary.net>
2025-03-30 00:04:33 -0500

    move participant logic to hook

--- ui/src/features/session/component/settingsSection/SettingsSection.tsx
+++ ui/src/features/session/component/settingsSection/SettingsSection.tsx
@@ -12,44 +12,21 @@ import ActionsSectionDropdown from "../../../components/actionsSection/ActionsSe
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from '@mui/icons-material/Remove';
 import {useSessionParticipant} from "../../hooks/useSessionParticipant";
-import {SessionParticipant} from "../../../../types";
 
 const SettingsSection = () => {
     const session = useAppSelector(selectSession);
 
     const [selectedActorId, setSelectedActorId] = useState<number>(null);
     const [selectedParticipantId, setSelectedParticipantId] = useState<number>(null);
-    const [participants, setParticipants] = useState<SessionParticipant[]>([]);
-
-    let participantMap = new Map();
 
     const { data: actors, isLoading: areActorsLoading } = useGetActorsQuery();
 
-    const { addParticipantToSession, deleteParticipantFromSession, getSessionParticipants } = useSessionParticipant();
+    const { addParticipantToSession, deleteSessionParticipant, participants  } = useSessionParticipant();
 
     useEffect(() => {
-        setSelectedActorId(0);
-        updateSessionParticipants();
+        setSelectedActorId(null);
+        setSelectedParticipantId(null);
     }, [session]);
-
-    useEffect(() => {
-        if (!Array.isArray(participants)) {
-            return;
-        }
-
-        for (const participant of participants) {
-            participantMap.set(participant.sessionParticipantId, participant);
-        }
-    }, [participants]);
-
-    const updateSessionParticipants = async () => {
-        if (!session?.sessionId) {
-            return;
-        }
-
-        const sessionParticipants = await getSessionParticipants(session.sessionId).unwrap();
-        setParticipants(sessionParticipants);
-    };
 
     const assistantDropdownItems = areActorsLoading ? [] : actors.map((actor, index) => (
         <MenuItem value={actor.actorId} key={`id-assistant-dropdown-item-${index}`}>{ actor.name }</MenuItem>
@@ -58,14 +35,6 @@ const SettingsSection = () => {
     const participantDropdownItems = participants.map((sessionParticipant, index) => (
         <MenuItem value={sessionParticipant.sessionParticipantId} key={`id-participants-dropdown-item-${index}`}>{ sessionParticipant.participant.name }</MenuItem>
     ));
-
-    const onDeleteSessionParticipant = async () => {
-        if (!participantMap.has(selectedParticipantId)) {
-            return;
-        }
-
-        await deleteParticipantFromSession(session.sessionId, participantMap.get(selectedParticipantId));
-    };
 
     return (
         <Grid >
@@ -99,7 +68,7 @@ const SettingsSection = () => {
                             label='Remove Participant'
                             value={selectedParticipantId}
                             items={participantDropdownItems}
-                            iconButton={<RemoveIcon onClick={onDeleteSessionParticipant} />}
+                            iconButton={<RemoveIcon onClick={() => deleteSessionParticipant(selectedParticipantId)} />}
                             onChange={e => setSelectedParticipantId(e.target.value as number)}
                             disabled={!session}
                         />
