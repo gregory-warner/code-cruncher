38987b006a66f5896b2bff94062af087a77dd30f
Greg Warner <cats-carrying-canes@canary.net>
2025-04-02 20:46:20 -0500

    remove unneeded annotation

--- services/prompt-service/src/main/java/com/psognathus/code/prompt_service/PromptServiceApplication.java
+++ services/prompt-service/src/main/java/com/psognathus/code/prompt_service/PromptServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
 @SpringBootApplication
 public class PromptServiceApplication {
 	public static void main(String[] args) {
