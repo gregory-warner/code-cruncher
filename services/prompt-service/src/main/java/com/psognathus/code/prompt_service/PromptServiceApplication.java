f51b3f46e01a01cec04cb3d787d6b53ffdbde63b
Greg Warner <cats-carrying-canes@canary.net>
2025-04-02 20:43:58 -0500

    add component scan and move controller to rest directory

--- services/prompt-service/src/main/java/com/psognathus/code/prompt_service/PromptServiceApplication.java
+++ services/prompt-service/src/main/java/com/psognathus/code/prompt_service/PromptServiceApplication.java
@@ -4,18 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
 public class PromptServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PromptServiceApplication.class, args);
-	}
-}
-
-@RestController
-class HelloController {
-	@GetMapping("/")
-	public String hello() {
-		return "hey";
 	}
 }
