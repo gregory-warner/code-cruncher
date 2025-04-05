f0c9cd088fc7105a48442dd6b6079fadb2f43d1d
Greg Warner <cats-carrying-canes@canary.net>
2025-04-05 09:33:58 -0500

    add findBy to repo

--- services/prompt-service/src/main/java/com/psognathus/code/prompt_service/prompt/PromptRepository.java
+++ services/prompt-service/src/main/java/com/psognathus/code/prompt_service/prompt/PromptRepository.java
@@ -2,6 +2,9 @@ package com.psognathus.code.prompt_service.prompt;
 
 import com.psognathus.code.prompt_service.prompt.Prompt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PromptRepository extends JpaRepository<Prompt, Long> {
+
+    List<Prompt> findByAssistantId(Long assistantId);
 }
