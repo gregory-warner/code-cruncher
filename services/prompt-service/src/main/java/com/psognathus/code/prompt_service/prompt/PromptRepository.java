6000e6caeff5c6378b3475951475152e45aba23d
Greg Warner <cats-carrying-canes@canary.net>
2025-04-05 20:36:17 -0500

    add query to get all prompts

--- services/prompt-service/src/main/java/com/psognathus/code/prompt_service/prompt/PromptRepository.java
+++ services/prompt-service/src/main/java/com/psognathus/code/prompt_service/prompt/PromptRepository.java
@@ -2,9 +2,14 @@ package com.psognathus.code.prompt_service.prompt;
 
 import com.psognathus.code.prompt_service.prompt.Prompt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PromptRepository extends JpaRepository<Prompt, Long> {
 
     List<Prompt> findByAssistantId(Long assistantId);
+
+    @Query("SELECT p from prompt p where p.assistant_id = :assistantId")
+    List<Prompt> findAllByAssistantId(@Param("assistantId") Long assistantId);
 }
